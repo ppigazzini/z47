1d234683c767f0533c40f957dcc9c2474341b7d4
jaymos <jaco@cocoon-creations.com>
2023-11-09 01:02:37 +0900

    version

--- src/wp43/defines.h
+++ src/wp43/defines.h
@@ -24,7 +24,7 @@
 // JM VARIOUS OPTIONS
 //*********************************
 
-#define VERSION1 "0.108.16.01-T2"     // major release . minor release . tracked build . internal OR un/tracked OR subrelease : Alpha / Beta / RC1
+#define VERSION1 "0.108.16.01 B1"     // major release . minor release . tracked build . internal OR un/tracked OR subrelease : Alpha / Beta / RC1
 
 //2023-11-04-0.108.16.00 Stable non-master
 
