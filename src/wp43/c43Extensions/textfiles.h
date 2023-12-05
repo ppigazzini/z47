344892cb3bb3e92629c0008db5cb56e24cc1b9b5
jaymos <jaco@cocoon-creations.com>
2023-12-05 19:01:50 +0200

    Add PRINTxy

--- src/wp43/c43Extensions/textfiles.h
+++ src/wp43/c43Extensions/textfiles.h
@@ -28,11 +28,13 @@
 #include "typeDefinitions.h"
 #include <stdint.h>
 
+#define ONELINE true
+
 void         print_inlinestr(const char *line1, bool_t endline);
 void         print_Register_line(calcRegister_t regist, char *before, char *after, bool_t line_init);
 void         displaywords(char *line1);
 int16_t      export_string_to_file(const char line1[TMP_STR_LENGTH]);
-void         stackregister_csv_out(int16_t reg_b, int16_t reg_e);
+void         stackregister_csv_out(int16_t reg_b, int16_t reg_e, bool_t oneLine);
 void         aimBuffer_csv_out(void);
 void         copyRegisterToClipboardString2(calcRegister_t regist, char *clipboardString);
 
