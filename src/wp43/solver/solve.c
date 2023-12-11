73b53d93b471898524c0503d16af0c94f73060ff
jaymos <jaco@cocoon-creations.com>
2023-12-12 01:38:41 +0200

    Import previous WP43 improvements in solver

--- src/wp43/solver/solve.c
+++ src/wp43/solver/solve.c
@@ -230,8 +230,25 @@ void fnSolveVar(uint16_t unusedButMandatoryParameter) {
   #if !defined(TESTSUITE_BUILD)
   const char *var = (char *)getNthString(dynamicSoftmenu[softmenuStack[0].softmenuId].menuContent, dynamicMenuItem);
   const uint16_t regist = findOrAllocateNamedVariable(var);
+  const uint16_t nameLength = stringByteLength(var) + 1;
   if(currentMvarLabel != INVALID_VARIABLE) {
-    reallyRunFunction(ITM_STO, regist);
+	if(currentSolverStatus & SOLVER_STATUS_INTERACTIVE) { // MNU_MVAR was displayed by the Solver
+		reallyRunFunction(ITM_STO, regist);
+	}
+	else {	// MNU_MVAR was displayed by VARMNU
+		if(entryStatus & 0x01) { // MVAR menu key pressed after a user entry: save the value in the variable
+			entryStatus &= 0xfe;
+			currentSolverVariable = regist;
+			reallyRunFunction(ITM_STO, regist);
+			temporaryInformation = TI_SOLVER_VARIABLE;
+		}
+		else { // MVAR menu key pressed without a a user entry: store the variable name in K and continue program execution 
+			reallocateRegister(REGISTER_K, dtString, nameLength , amNone);
+			xcopy(REGISTER_STRING_DATA(REGISTER_K), var, nameLength );
+			dynamicMenuItem = -1;
+			runProgram(false, INVALID_VARIABLE);
+		}
+	}
   }
   else if((currentSolverStatus & SOLVER_STATUS_EQUATION_MODE) == SOLVER_STATUS_EQUATION_1ST_DERIVATIVE || (currentSolverStatus & SOLVER_STATUS_EQUATION_MODE) == SOLVER_STATUS_EQUATION_2ND_DERIVATIVE) {
     currentSolverVariable = regist;
@@ -259,8 +276,10 @@ void fnSolveVar(uint16_t unusedButMandatoryParameter) {
       parseEquation(currentFormula, EQUATION_PARSER_XEQ, tmpString, tmpString + AIM_BUFFER_LENGTH);
     }
     else {
+      uint16_t savedCurrentSolverProgram = currentSolverProgram;
       dynamicMenuItem = -1;
       execProgram(currentSolverProgram + FIRST_LABEL);
+      currentSolverProgram = savedCurrentSolverProgram;
     }
     if(lastErrorCode == ERROR_OVERFLOW_PLUS_INF) {
       realToReal34(const_plusInfinity, res);
@@ -341,10 +360,9 @@ static void _executeSolver(calcRegister_t variable, const real34_t *val, real34_
     realDivide(const_1, &den, &den, realContext);
     realFMA(&num, &den, &val, res, realContext);
   }
-#endif // !TESTSUITE_BUILD
 
 
-#if !defined(TESTSUITE_BUILD)
+
   static void _showProgress(const real34_t *a, const real34_t *b, const real34_t *fa, const real34_t *fb) {
     #if ENABLE_SOLVER_PROGRESS == 1
         const real34_t *c;
