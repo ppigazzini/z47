ddb49eb5b289af8493617391307074db0830451d
Pauli <pauli@openssl.org>
2024-01-09 12:48:00 +1100

    Update inverse hyperbolic functions to handle short integers

--- src/wp43/mathematics/arccosh.c
+++ src/wp43/mathematics/arccosh.c
@@ -36,133 +36,11 @@
 #include "wp43.h"
 
 
-
-TO_QSPI void (* const arccosh[NUMBER_OF_DATA_TYPES_FOR_CALCULATIONS])(void) = {
-// regX ==> 1            2            3            4             5             6             7            8            9             10
-//          Long integer Real34       Complex34    Time          Date          String        Real34 mat   Complex34 m  Short integer Config data
-            arccoshLonI, arccoshReal, arccoshCplx, arccoshError, arccoshError, arccoshError, arccoshRema, arccoshCxma, arccoshError, arccoshError
-};
-
-
-
-/********************************************//**
- * \brief Data type error in arccosh
- *
- * \param void
- * \return void
- ***********************************************/
-#if(EXTRA_INFO_ON_CALC_ERROR == 1)
-  void arccoshError(void) {
-    displayCalcErrorMessage(ERROR_INVALID_DATA_TYPE_FOR_OP, ERR_REGISTER_LINE, REGISTER_X);
-    sprintf(errorMessage, "cannot calculate arccosh for %s", getRegisterDataTypeName(REGISTER_X, true, false));
-    moreInfoOnError("In function fnArccos:", errorMessage, NULL, NULL);
-  }
-#endif // (EXTRA_INFO_ON_CALC_ERROR == 1)
-
-
-
-/********************************************//**
- * \brief regX ==> regL and arccosh(regX) ==> regX
- * enables stack lift and refreshes the stack
- *
- * \param[in] unusedButMandatoryParameter uint16_t
- * \return void
- ***********************************************/
-void fnArccosh(uint16_t unusedButMandatoryParameter) {
-  if(!saveLastX()) {
-    return;
-  }
-
-  arccosh[getRegisterDataType(REGISTER_X)]();
-
-  adjustResult(REGISTER_X, false, true, REGISTER_X, -1, -1);
-}
-
-
-
-void arccoshLonI(void) {
-  real_t x;
-
-  convertLongIntegerRegisterToReal(REGISTER_X, &x, &ctxtReal39);
-  if(realCompareLessThan(&x, const_1)) {
-    if(getFlag(FLAG_CPXRES)) {
-      reallocateRegister(REGISTER_X, dtComplex34, COMPLEX34_SIZE_IN_BLOCKS, amNone);
-      convertRealToReal34ResultRegister(&x, REGISTER_X);
-      real34Zero(REGISTER_IMAG34_DATA(REGISTER_X));
-      arccoshCplx();
-    }
-    else if(getSystemFlag(FLAG_SPCRES)) {
-      reallocateRegister(REGISTER_X, dtReal34, REAL34_SIZE_IN_BLOCKS, amNone);
-      convertRealToReal34ResultRegister(const_NaN, REGISTER_X);
-    }
-    else {
-      displayCalcErrorMessage(ERROR_ARG_EXCEEDS_FUNCTION_DOMAIN, ERR_REGISTER_LINE, REGISTER_X);
-      #if(EXTRA_INFO_ON_CALC_ERROR == 1)
-        moreInfoOnError("In function arccoshLonI:", "X < 1", "and CPXRES is not set!", NULL);
-      #endif // (EXTRA_INFO_ON_CALC_ERROR == 1)
-    }
-    return;
-  }
-
-  reallocateRegister(REGISTER_X, dtReal34, REAL34_SIZE_IN_BLOCKS, amNone);
-
-  realArcosh(&x, &x, &ctxtReal39);
-
-  convertRealToReal34ResultRegister(&x, REGISTER_X);
-}
-
-
-
-void arccoshRema(void) {
-  elementwiseRema(arccoshReal);
-}
-
-
-
-void arccoshCxma(void) {
-  elementwiseCxma(arccoshCplx);
-}
-
-
-
-void arccoshReal(void) {
-  real_t x;
-
-  real34ToReal(REGISTER_REAL34_DATA(REGISTER_X), &x);
-
-  if(realCompareLessThan(&x, const_1)) {
-    if(getFlag(FLAG_CPXRES)) {
-      reallocateRegister(REGISTER_X, dtComplex34, COMPLEX34_SIZE_IN_BLOCKS, amNone);
-      convertRealToReal34ResultRegister(&x, REGISTER_X);
-      real34Zero(REGISTER_IMAG34_DATA(REGISTER_X));
-      arccoshCplx();
-      setRegisterAngularMode(REGISTER_X, amNone);
-    }
-    else if(getSystemFlag(FLAG_SPCRES)) {
-      convertRealToReal34ResultRegister(const_NaN, REGISTER_X);
-    }
-    else {
-      displayCalcErrorMessage(ERROR_ARG_EXCEEDS_FUNCTION_DOMAIN, ERR_REGISTER_LINE, REGISTER_X);
-      #if(EXTRA_INFO_ON_CALC_ERROR == 1)
-        moreInfoOnError("In function arccoshReal:", "X < 1", "and CPXRES is not set!", NULL);
-      #endif // (EXTRA_INFO_ON_CALC_ERROR == 1)
-    }
-    return;
-  }
-
-  realArcosh(&x, &x, &ctxtReal75);
-
-  convertRealToReal34ResultRegister(&x, REGISTER_X);
-  setRegisterAngularMode(REGISTER_X, amNone);
-}
-
-
-
-void arccoshCplx(void) {
+static void arccoshCplx(void) {
   real_t a, b, real, imag;
 
-  real34ToReal(REGISTER_REAL34_DATA(REGISTER_X), &a);
-  real34ToReal(REGISTER_IMAG34_DATA(REGISTER_X), &b);
+  if (!getRegisterAsComplex(REGISTER_X, &a, &b))
+    return;
 
   // arccosh(z) = ln(z + sqrt(z² - 1))
   // calculate z²   real part
@@ -195,6 +73,36 @@ void arccoshCplx(void) {
 }
 
 
+static void arccoshReal(void) {
+  real_t x;
+  const real_t *r = &x;
+
+  if (!getRegisterAsReal(REGISTER_X, &x))
+    return;
+
+  if(realCompareLessThan(&x, const_1)) {
+    if(getFlag(FLAG_CPXRES)) {
+      arccoshCplx();
+      return;
+    }
+    if(getSystemFlag(FLAG_SPCRES)) {
+      r = const_NaN;
+    }
+    else {
+      displayCalcErrorMessage(ERROR_ARG_EXCEEDS_FUNCTION_DOMAIN, ERR_REGISTER_LINE, REGISTER_X);
+      #if(EXTRA_INFO_ON_CALC_ERROR == 1)
+        moreInfoOnError("In function arccoshReal:", "X < 1", "and CPXRES is not set!", NULL);
+      #endif // (EXTRA_INFO_ON_CALC_ERROR == 1)
+      return;
+    }
+  } else
+    realArcosh(&x, &x, &ctxtReal75);
+
+  reallocateRegister(REGISTER_X, dtReal34, REAL34_SIZE_IN_BLOCKS, amNone);
+  convertRealToReal34ResultRegister(r, REGISTER_X);
+}
+
+
 
 void realArcosh(const real_t *x, real_t *res, realContext_t *realContext) {
   real_t xSquared;
@@ -205,4 +113,17 @@ void realArcosh(const real_t *x, real_t *res, realContext_t *realContext) {
   realSquareRoot(&xSquared, &xSquared, realContext);
   realAdd(&xSquared, x, res, realContext);
   WP34S_Ln(res, res, realContext);
+}
+
+
+
+/********************************************//**
+ * \brief regX ==> regL and arccosh(regX) ==> regX
+ * enables stack lift and refreshes the stack
+ *
+ * \param[in] unusedButMandatoryParameter uint16_t
+ * \return void
+ ***********************************************/
+void fnArccosh(uint16_t unusedButMandatoryParameter) {
+  processRealComplexMonadicFunction(&arccoshReal, &arccoshCplx);
 }
--- src/wp43/mathematics/arccosh.h
+++ src/wp43/mathematics/arccosh.h
@@ -26,16 +26,5 @@
 
   void fnArccosh   (uint16_t unusedButMandatoryParameter);
 
-  #if(EXTRA_INFO_ON_CALC_ERROR == 1)
-    void arccoshError(void);
-  #else // (EXTRA_INFO_ON_CALC_ERROR == 1)
-    #define arccoshError typeError
-  #endif // (EXTRA_INFO_ON_CALC_ERROR == 1)
-
-  void arccoshLonI (void);
-  void arccoshRema (void);
-  void arccoshCxma (void);
-  void arccoshReal (void);
-  void arccoshCplx (void);
   void realArcosh  (const real_t *x, real_t *res, realContext_t *realContext);
 #endif // !ARCCOSH_H
