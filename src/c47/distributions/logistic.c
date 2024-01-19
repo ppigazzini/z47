7c3c927111134bb91c43294f6168c2850886e523
Pauli <pauli@openssl.org>
2024-01-19 21:55:51 +1100

    Change logistic distribution parameters to registers M & S

--- src/c47/distributions/logistic.c
+++ src/c47/distributions/logistic.c
@@ -45,8 +45,8 @@
 #else
   static bool_t checkParamLogistic(real_t *x, real_t *i, real_t *j) {
     if (!getRegisterAsReal(REGISTER_X, x)
-        || !getRegisterAsReal(REGISTER_STAT1, i)
-        || !getRegisterAsReal(REGISTER_STAT2, j))
+        || !getRegisterAsReal(REGISTER_M, i)
+        || !getRegisterAsReal(REGISTER_S, j))
       goto err;
 
     if(realIsZero(j) || realIsNegative(j)) {
