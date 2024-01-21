b0b61d634e2f07f57d8d6a9fe72123f5595a2ded
Pauli <pauli@openssl.org>
2024-01-22 09:44:07 +1100

    Update Euler's formula to use helpers

--- src/c47/mathematics/eulersFormula.c
+++ src/c47/mathematics/eulersFormula.c
@@ -37,46 +37,6 @@
 
 #include "c47.h"
 
-
-
-TO_QSPI void (* const dispatch_eulersFormula[NUMBER_OF_DATA_TYPES_FOR_CALCULATIONS])(void) = {
-// regX ==> 1                     2                  3                  4                   5                   6                   7                   8                   9                   10
-//          Long integer          Real34             complex34          Time                Date                String              Real34 mat          Complex34 m         Short integer       Config data
-            eulersFormulaLongint, eulersFormulaReal, eulersFormulaCplx, eulersFormulaError, eulersFormulaError, eulersFormulaError, eulersFormulaRema,  eulersFormulaCxma,  eulersFormulaError, eulersFormulaError
-};
-
-
-#if(EXTRA_INFO_ON_CALC_ERROR == 1)
-  void eulersFormulaError(void) {
-    displayCalcErrorMessage(ERROR_INVALID_DATA_TYPE_FOR_OP, ERR_REGISTER_LINE, REGISTER_X);
-    sprintf(errorMessage, "cannot calculate e^ix for %s", getRegisterDataTypeName(REGISTER_X, true, false));
-    moreInfoOnError("In function fnEulersFormula:", errorMessage, NULL, NULL);
-  }
-#endif // (EXTRA_INFO_ON_CALC_ERROR == 1)
-
-
-
-void fnEulersFormula(uint16_t unusedButMandatoryParameter) {
-  if(!saveLastX()) {
-    return;
-  }
-  dispatch_eulersFormula[getRegisterDataType(REGISTER_X)]();
-}
-
-
-
-void eulersFormulaRema(void) {
-  elementwiseRema(eulersFormulaReal);
-}
-
-
-
-void eulersFormulaCxma(void) {
-  elementwiseCxma(eulersFormulaCplx);
-}
-
-
-
 void eulersFormula(const real_t *inReal, const real_t *inImag, real_t *outReal, real_t *outImag, realContext_t *ctxt) {
   real_t zReal, zImag;
 
@@ -84,9 +44,13 @@ void eulersFormula(const real_t *inReal, const real_t *inImag, real_t *outReal, 
   expComplex(&zReal, &zImag, outReal, outImag, ctxt);
 }
 
-void eulersFormulaCplx(void) {
+static void eulersFormulaCplx(void) {
+  real_t zReal, zImag;
 
-  if( (real34IsInfinite(REGISTER_REAL34_DATA(REGISTER_X)) || (real34IsInfinite(REGISTER_IMAG34_DATA(REGISTER_X)))) ) {
+  if (!getRegisterAsComplex(REGISTER_X, &zReal, &zImag))
+    return;
+
+  if( (realIsInfinite(&zReal) || (real34IsInfinite(&zImag))) ) {
     if(!getSystemFlag(FLAG_SPCRES)) {
       displayCalcErrorMessage(ERROR_ARG_EXCEEDS_FUNCTION_DOMAIN, ERR_REGISTER_LINE, REGISTER_X);
       #if(EXTRA_INFO_ON_CALC_ERROR == 1)
@@ -94,29 +58,27 @@ void eulersFormulaCplx(void) {
       #endif // (EXTRA_INFO_ON_CALC_ERROR == 1)
       return;
     }
-    else {
-      convertComplexToResultRegister(const_NaN, const_NaN, REGISTER_X);
-      fnSetFlag(FLAG_CPXRES);
-      fnRefreshState();
-    }
+    convertComplexToResultRegister(const_NaN, const_NaN, REGISTER_X);
+    goto end;
   }
 
-  real_t zReal, zImag;
-
-  fnSetFlag(FLAG_CPXRES);
-  fnRefreshState();
-  real34ToReal(REGISTER_REAL34_DATA(REGISTER_X), &zReal);
-  real34ToReal(REGISTER_IMAG34_DATA(REGISTER_X), &zImag);
-
   eulersFormula(&zReal, &zImag, &zReal, &zImag, &ctxtReal39);
-
   convertComplexToResultRegister(&zReal, &zImag, REGISTER_X);
 
+end:
+  fnSetFlag(FLAG_CPXRES);
+  fnRefreshState();
 }
 
+static void eulersFormulaReal(void) {
+  real_t c, i;
+  angularMode_t xAngularMode;
+  const uint32_t type = getRegisterDataType(REGISTER_X);
 
-void eulersFormulaReal(void) {
-  if((real34IsInfinite(REGISTER_REAL34_DATA(REGISTER_X))) && !getSystemFlag(FLAG_SPCRES)) {
+  if (!getRegisterAsReal(REGISTER_X, &c))
+    return;
+
+  if(realIsInfinite(&c) && !getSystemFlag(FLAG_SPCRES)) {
     displayCalcErrorMessage(ERROR_ARG_EXCEEDS_FUNCTION_DOMAIN, ERR_REGISTER_LINE, REGISTER_X);
     #if(EXTRA_INFO_ON_CALC_ERROR == 1)
       moreInfoOnError("In function eulersFormulaReal:", "cannot use " STD_PLUS_MINUS STD_INFINITY " as X input when flag D is not set", NULL, NULL);
@@ -124,32 +86,19 @@ void eulersFormulaReal(void) {
     return;
   }
 
-  real_t c, i;
-
-  fnSetFlag(FLAG_CPXRES);
-  fnRefreshState();
-  if(getRegisterAngularMode(REGISTER_X) != amNone) {
-    fnCvtFromCurrentAngularMode(amRadian);
-    setRegisterAngularMode(REGISTER_X, amNone);
+  if (type == dtReal34) {
+    xAngularMode = getRegisterAngularMode(REGISTER_X);
+    if (xAngularMode != amNone)
+      convertAngleFromTo(&c, xAngularMode, amRadian, &ctxtReal39);
   }
-  real34ToReal(REGISTER_REAL34_DATA(REGISTER_X), &c);
-  reallocateRegister(REGISTER_X, dtComplex34, COMPLEX34_SIZE_IN_BLOCKS, amNone);
 
   eulersFormula(&c, const_0, &c, &i, &ctxtReal39);
 
   convertComplexToResultRegister(&c, &i, REGISTER_X);
+  fnSetFlag(FLAG_CPXRES);
+  fnRefreshState();
 }
 
-
-void eulersFormulaLongint(void) {
-  real_t c, i;
-
-  fnSetFlag(FLAG_CPXRES);
-  fnRefreshState();
-  convertLongIntegerRegisterToReal(REGISTER_X, &c, &ctxtReal39);
-  reallocateRegister(REGISTER_X, dtComplex34, COMPLEX34_SIZE_IN_BLOCKS, amNone);
-
-  eulersFormula(&c, const_0, &c, &i, &ctxtReal39);
-
-  convertComplexToResultRegister(&c, &i, REGISTER_X);
+void fnEulersFormula(uint16_t unusedButMandatoryParameter) {
+  processRealComplexMonadicFunction(&eulersFormulaReal, &eulersFormulaCplx);
 }
--- src/c47/mathematics/eulersFormula.h
+++ src/c47/mathematics/eulersFormula.h
@@ -26,19 +26,6 @@
 #include <stdint.h>
 
 void fnEulersFormula(uint16_t unusedButMandatoryParameter);
-
-#if(EXTRA_INFO_ON_CALC_ERROR == 1)
-void eulersFormulaError  (void);
-  #else // (EXTRA_INFO_ON_CALC_ERROR != 1)
-#define eulersFormulaError typeError
-#endif // (EXTRA_INFO_ON_CALC_ERROR == 1)
-
-void eulersFormulaCplx       (void);
-void eulersFormulaReal       (void);
-void eulersFormulaLongint    (void);
-void eulersFormulaRema       (void);
-void eulersFormulaCxma       (void);
-
 void eulersFormula(const real_t *inReal, const real_t *inImag, real_t *outReal, real_t *outImag, realContext_t *ctxt);
 
 #endif // !EULERSFORMULA_H
