8cf66d69caffd379cec0741782bbca05a8378563
Pauli <pauli@openssl.org>
2024-01-24 09:22:22 +1100

    Convert FIB to helpers

--- src/c47/mathematics/fib.h
+++ src/c47/mathematics/fib.h
@@ -25,17 +25,4 @@
   #include <stdint.h>
 
   void fnFib   (uint16_t unusedButMandatoryParameter);
-
-  #if(EXTRA_INFO_ON_CALC_ERROR == 1)
-    void fibError(void);
-  #else // (EXTRA_INFO_ON_CALC_ERROR != 1)
-    #define fibError typeError
-  #endif // (EXTRA_INFO_ON_CALC_ERROR == 1)
-
-  void fibLonI (void);
-  void fibReal (void);
-  void fibCplx (void);
-
-  uint8_t FibonacciReal(const real_t *n, real_t *res, realContext_t *realContext);
-  uint8_t FibonacciComplex(const real_t *nReal, const real_t *nImag, real_t *resReal, real_t *resImag, realContext_t *realContext);
 #endif // !FIB_H
