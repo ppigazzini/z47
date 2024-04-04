6f6a0c44d005ecf10c910b43fd8a0dfea5c11dd7
jaymos <jaco@cocoon-creations.com>
2024-04-05 00:41:50 +0200

    version

--- src/c47/defines.h
+++ src/c47/defines.h
@@ -14,7 +14,7 @@
 // JM VARIOUS OPTIONS
 //*********************************
 
-#define VERSION1 "0.109.01.01a"     // major release . minor release . tracked build . internal OR un/tracked OR subrelease : Alpha / Beta / RC1
+#define VERSION1 "0.109.01.02a"     // major release . minor release . tracked build . internal OR un/tracked OR subrelease : Alpha / Beta / RC1
 
 #define MODEL USER_C47               // USER_C47 or USER_R47
 
