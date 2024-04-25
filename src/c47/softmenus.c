b1db3c2bd3bcb2e6425e6a25ee25960719831aca
jaymos <jaco@cocoon-creations.com>
2024-04-25 21:48:49 +0200

    Make-menu-items-also-cross-out-if-not-available-(needed-for-TVM)

--- src/c47/softmenus.c
+++ src/c47/softmenus.c
@@ -1827,6 +1827,7 @@ void changeSoftKey(int16_t menuNr, int16_t itemNr, char * itemName, videoMode_t 
                           default: ;
                           }
                           break;
+
       case ITM_SCR    :switch(*showValue) {
                           case NC_NORMAL      : *showValue = NOVAL; break;
                           case NC_SUBSCRIPT   : stringAppend(showText + stringByteLength(showText), alphaCase == AC_LOWER ? STD_SUB_s STD_SUB_u STD_SUB_b : alphaCase == AC_UPPER ? STD_SUB_S STD_SUB_U STD_SUB_B : ""); *showValue = NOVAL;
@@ -1897,14 +1898,35 @@ void changeSoftKey(int16_t menuNr, int16_t itemNr, char * itemName, videoMode_t 
 
 
 void fnStrikeOutIfNotCoded(int16_t itemNr, int16_t x, int16_t y) {
-  if(itemNr > 0 && indexOfItems[itemNr%10000].func == itemToBeCoded) {
+  int16_t strike = 0;
+  if(itemNr > 0) {
+    if (indexOfItems[itemNr%10000].func == itemToBeCoded/* || savedspace(itemNr)*/) {
+      strike = 1;
+    }
+  } else {
+    int16_t m = 0;
+    while(softmenu[m].menuItem != 0) {
+      if(softmenu[m].menuItem == itemNr%10000) {
+       break;
+      }
+      m++;
+    }
+    if (softmenu[m].numItems == 0) {
+      strike = -1;
+    }
+  }
+  if(strike != 0) {
     // Strike out non coded functions
     int16_t yStroke = SCREEN_HEIGHT - y*23 - 1;
     for(int16_t xStroke=x*67 + 1 +9 ; xStroke<x*67 + 66 -10; xStroke++) {      //JM mod stroke slash cross out
       if(xStroke%3 == 0) {
         yStroke--;
       }
-      setBlackPixel(xStroke, yStroke -3);                                      //JM mod
+      if(strike == 1) {
+        setBlackPixel(xStroke, yStroke -3);                                      //JM mod
+      } else {
+        setWhitePixel(xStroke, yStroke -3);                                      //JM mod        
+      }
     }
   }
 }
@@ -2221,9 +2243,8 @@ bool_t BASE_OVERRIDEONCE = false;
               int16_t xStrokeA=x*67 + 66 -12;
               plotline(xStrokeA +2+4, yStrokeA -16-3-1, xStrokeA +2+4+5-1, yStrokeA -16-3+5);
             }
-            fnStrikeOutIfNotCoded(item%10000, x, y-currentFirstItem/6);
-
           }
+          fnStrikeOutIfNotCoded(item%10000, x, y-currentFirstItem/6);
         }
       }
 
@@ -2610,12 +2631,13 @@ bool_t BASE_OVERRIDEONCE = false;
         }
       }
       else {
-      parseEquation(currentFormula, EQUATION_PARSER_MVAR, aimBuffer, tmpString);
+        parseEquation(currentFormula, EQUATION_PARSER_MVAR, aimBuffer, tmpString);
         varList = (uint8_t *)tmpString;
       }
       id = -MNU_MVAR;
       while((getNthString(varList, ++numberOfVars))[0] != 0) {
       }
+
       if(numberOfVars > 12) {
         displayCalcErrorMessage(ERROR_EQUATION_TOO_COMPLEX, ERR_REGISTER_LINE, NIM_REGISTER_LINE);
         #if(EXTRA_INFO_ON_CALC_ERROR == 1)
