4d2e3bfc4bcfd968cdcb26783bc4045e378788c6
jaymos <jaco@cocoon-creations.com>
2024-06-18 22:13:04 +0200

    Fix bug add error for prerequisite variable for EQN menus

--- src/c47/error.c
+++ src/c47/error.c
@@ -94,30 +94,31 @@ TO_QSPI const char errorMessages[NUMBER_OF_ERROR_CODES][SIZE_OF_EACH_ERROR_MESSA
 /* 54 */  "No program specified",
 /* 55 */  "Cannot write file ",
 /* 56 */  "Function has changed, please replace",
+/* 57 */  "Variable required, please select variable",
 
 //Status output messages for time consuming tasks, to keep user informed
-/* 57 */  "  Loading state file ...",
-/* 58 */  "  Saving state file ...",
-/* 59 */  "  Loading stats ...",
-/* 60 */  "  Solving for real/complex root ...",
-/* 61 */  "  Calculating graph coordinates ...",
-/* 62 */  "  Re-calculating sums ... ",
-/* 63 */  "  Solving for real root ...",
+/* 58 */  "  Loading state file ...",
+/* 59 */  "  Saving state file ...",
+/* 60 */  "  Loading stats ...",
+/* 61 */  "  Solving for real/complex root ...",
+/* 62 */  "  Calculating graph coordinates ...",
+/* 63 */  "  Re-calculating sums ... ",
+/* 64 */  "  Solving for real root ...",
 
 //TI Messages (incomplete)
-/* 64 */  "Backup restored",
-/* 65 */  "State file loaded",
-/* 66 */  "Programs and equations loaded",
-/* 67 */  "appended",
-/* 68 */  "Global and local registers loaded",
-/* 69 */  "(w/ local flags)",
-/* 70 */  "System settings loaded",
-/* 71 */  "Statistical data loaded",
-/* 72 */  "User variables loaded",
-/* 73 */  "Program file loaded",
-/* 74 */  "Not enough memory for undo",
+/* 65 */  "Backup restored",
+/* 66 */  "State file loaded",
+/* 67 */  "Programs and equations loaded",
+/* 68 */  "appended",
+/* 69 */  "Global and local registers loaded",
+/* 70 */  "(w/ local flags)",
+/* 71 */  "System settings loaded",
+/* 72 */  "Statistical data loaded",
+/* 73 */  "User variables loaded",
+/* 74 */  "Program file loaded",
+/* 75 */  "Not enough memory for undo",
 
-/* 75 */
+/* 76 */
 
 };
 
