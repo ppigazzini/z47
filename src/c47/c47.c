b238fdb39ce1ac63848f6e71b35b02f5509b3c4d
jaymos <jaco@cocoon-creations.com>
2024-07-10 23:29:21 +0200

    Step 2a fixup

--- src/c47/c47.c
+++ src/c47/c47.c
@@ -186,9 +186,7 @@ bool_t                doRefreshSoftMenu;                       //dr
 bool_t                BASE_MYM;                                //JM Screen / keyboard operation setup
 bool_t                jm_G_DOUBLETAP;                          //JM Screen / keyboard operation setup
 bool_t                jm_LARGELI;
-bool_t                constantFractions;                       //JM
 uint8_t               IrFractionsCurrentStatus;
-bool_t                constantFractionsOn;                     //JM
 bool_t                tvmIKnown;
 bool_t                tvmIChanges;
 
