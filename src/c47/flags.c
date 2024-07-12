5f6e37a190ce28ba29d92f36462aeab6a2b0c73c
jaymos <jaco@cocoon-creations.com>
2024-07-12 20:46:11 +0200

    Step 4a fixup FRCYC FRCSRN

--- src/c47/flags.c
+++ src/c47/flags.c
@@ -70,13 +70,6 @@ static void systemFlagAction(uint16_t systemFlag, flagAction_t action) {
     default:                                               break;
   }
 
-// this will clash with the button! maybe have straight flags only  if(systemFlag == FLAG_IRFRAC && (action == FLAG_SET || (action == FLAG_FLIP && !getSystemFlag(FLAG_IRFRAC)))) {
-// this will clash with the button! maybe have straight flags only    setSystemFlag(FLAG_IRF_ON);
-// this will clash with the button! maybe have straight flags only  } 
-// this will clash with the button! maybe have straight flags only  else if(systemFlag == FLAG_IRFRAC && (action == FLAG_CLEAR || (action == FLAG_FLIP && getSystemFlag(FLAG_IRFRAC)))) {
-// this will clash with the button! maybe have straight flags only    clearSystemFlag(FLAG_IRF_ON);
-// this will clash with the button! maybe have straight flags only  } 
-
   switch(systemFlag) {
     case FLAG_YMD:
     case FLAG_DMY:
@@ -98,6 +91,8 @@ static void systemFlagAction(uint16_t systemFlag, flagAction_t action) {
     case FLAG_NUMLOCK:
     case FLAG_CPXMULT:
     case FLAG_ERPN:
+    case ITM_FRCYC: 
+    case ITM_FRCSRN:
     case FLAG_LARGELI:
     case FLAG_IRFRAC:
     case FLAG_IRF_ON:
@@ -649,6 +644,8 @@ void SetSetting(uint16_t jmConfig) {
     case ITM_PRTACT0: fnClearFlag(FLAG_PRTACT);                              break;
     case JC_FRC:      fnFlipFlag(FLAG_FRCSRN);                               break; //bit
     case JC_ERPN:     fnFlipFlag(FLAG_ERPN);                                 break; //   eRPN = !eRPN;              fnRefreshState(); break;
+    case ITM_FRCYC:   fnFlipFlag(FLAG_FRCYC);                                break; //  
+    case ITM_FRCSRN:  fnFlipFlag(FLAG_FRCSRN);                               break; //  
     case JC_LARGELI:  fnFlipFlag(FLAG_LARGELI);                              break; //   jm_LARGELI = !jm_LARGELI;  fnRefreshState(); break;
     case JC_CPXMULT:  fnFlipFlag(FLAG_CPXMULT);                              break; //   CPXMULT = !CPXMULT;        fnRefreshState(); break;
     case JC_NL:       fnFlipFlag(FLAG_NUMLOCK); showAlphaModeonGui();        break; //   numLock = !numLock;        showAlphaModeonGui(); break; //call numlock
