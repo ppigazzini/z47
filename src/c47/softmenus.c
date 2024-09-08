d237fd759f8a52df1cc17557088fd78f0af8d070
jaymos <jaco@cocoon-creations.com>
2024-09-08 23:38:16 +0200

    fixup

--- src/c47/softmenus.c
+++ src/c47/softmenus.c
@@ -530,7 +530,7 @@ TO_QSPI const int16_t menu_alphaMisc[]    ={ ITM_CR,                         ITM
                                              ITM_TILDE,                      ITM_HOURGLASS,                  ITM_WATCH,                      ITM_TIMER_SYMBOL,               ITM_NEG_EXCLAMATION_MARK,       ITM_USER_MODE,                 
                                              ITM_BATTERY,                    ITM_PRINTER,                    ITM_HAMBURGER,                  ITM_BST_char,                   ITM_SST_char,                   ITM_CYCLIC,                    
                                                        
-                                             ITM_USB_SYMBOL,                 ITM_SUB_SUN,                    ITM_SUB_EARTH,                  ITM_CR,                         ITM_US,                         ITM_UK,                        
+                                             ITM_USB_SYMBOL,                 ITM_SUB_SUN,                    ITM_SUB_EARTH,                  ITM_US,                         ITM_UK,                         ITM_NULL,
                                              ITM_NULL,                       ITM_NULL,                       ITM_NULL,                       ITM_NULL,                       ITM_NULL,                       ITM_NULL,                      
                                              ITM_NULL,                       ITM_NULL,                       ITM_NULL,                       ITM_NULL,                       ITM_NULL,                       ITM_NULL                        };
 
