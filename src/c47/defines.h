8492e923f74d476d3e7824c2387a8cb7e8001e73
jaymos <jaco@cocoon-creations.com>
2024-09-11 23:00:39 +0200

    version

--- src/c47/defines.h
+++ src/c47/defines.h
@@ -11,7 +11,7 @@
 // JM VARIOUS OPTIONS
 //*********************************
 
-#define VERSION1 "0.109.02.06b3"       // major release . minor release . tracked build . internal OR un/tracked OR subrelease : Alpha / Beta / RC1
+#define VERSION1 "0.109.02.06b4"       // major release . minor release . tracked build . internal OR un/tracked OR subrelease : Alpha / Beta / RC1
 
 //Version history
 //0.109.02.00
