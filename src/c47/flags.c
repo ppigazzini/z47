bc55bef231358684f60a8f03f5a2e6998cc7f47e
Didier <didier.lachieze@libertysurf.fr>
2024-10-19 06:26:16 +0200

    Remove lettered flags shortcuts to system flags

--- src/c47/flags.c
+++ src/c47/flags.c
@@ -37,39 +37,7 @@
 
 typedef enum { FLAG_CLEAR=0, FLAG_SET=1, FLAG_FLIP=2 } flagAction_t;
 
-static void systemFlagProcess(unsigned int idx, flagAction_t action) {
-  const unsigned int n = idx / 16;
-  const unsigned int b = idx % 16;
-
-  switch(action) {
-    case FLAG_CLEAR: {
-      globalFlags[n] &= ~(1u << b);
-      break;
-    }
-    case FLAG_SET: {
-      globalFlags[n] |=   1u << b;
-      break;
-    }
-    case FLAG_FLIP: {
-      globalFlags[n] ^=   1u << b;
-      break;
-    }
-  }
-}
-
 static void systemFlagAction(uint16_t systemFlag, flagAction_t action) {
-  switch(systemFlag) {
-    case FLAG_ENGOVR:   systemFlagProcess(FLAG_A, action); break;
-    case FLAG_OVERFLOW: systemFlagProcess(FLAG_B, action); break;
-    case FLAG_CARRY:    systemFlagProcess(FLAG_C, action); break;
-    case FLAG_SPCRES:   systemFlagProcess(FLAG_D, action); break;
-    case FLAG_CPXRES:   systemFlagProcess(FLAG_I, action); break;
-    case FLAG_LEAD0:    systemFlagProcess(FLAG_L, action); break;
-    case FLAG_TRACE:    systemFlagProcess(FLAG_T, action); break;
-    case FLAG_POLAR:    systemFlagProcess(FLAG_X, action); break;
-    default:                                               break;
-  }
-
   switch(systemFlag) {
     case FLAG_YMD:       //these flags need to update the corresponding softkey status
     case FLAG_DMY:
@@ -308,17 +276,7 @@ void fnSetFlag(uint16_t flag) {
   }
 
   else if(flag <= FLAG_K) { // Global flag
-    switch(flag) {
-      case FLAG_A: setSystemFlag(FLAG_ENGOVR);   break;
-      case FLAG_B: setSystemFlag(FLAG_OVERFLOW); break;
-      case FLAG_C: setSystemFlag(FLAG_CARRY);    break;
-      case FLAG_D: setSystemFlag(FLAG_SPCRES);   break;
-      case FLAG_I: setSystemFlag(FLAG_CPXRES);   break;
-      case FLAG_L: setSystemFlag(FLAG_LEAD0);    break;
-      case FLAG_T: setSystemFlag(FLAG_TRACE);    break;
-      case FLAG_X: setSystemFlag(FLAG_POLAR);    break;
-      default: globalFlags[flag/16] |= 1u << (flag%16);
-    }
+    globalFlags[flag/16] |= 1u << (flag%16);
   }
 
   else if(flag <= LAST_LOCAL_FLAG) { // Local flag
@@ -391,17 +349,7 @@ void fnClearFlag(uint16_t flag) {
   }
 
   else if(flag <= FLAG_K) { // Global flag
-    switch(flag) {
-      case FLAG_A: clearSystemFlag(FLAG_ENGOVR);   break;
-      case FLAG_B: clearSystemFlag(FLAG_OVERFLOW); break;
-      case FLAG_C: clearSystemFlag(FLAG_CARRY);    break;
-      case FLAG_D: clearSystemFlag(FLAG_SPCRES);   break;
-      case FLAG_I: clearSystemFlag(FLAG_CPXRES);   break;
-      case FLAG_L: clearSystemFlag(FLAG_LEAD0);    break;
-      case FLAG_T: clearSystemFlag(FLAG_TRACE);    break;
-      case FLAG_X: clearSystemFlag(FLAG_POLAR);    break;
-      default:     globalFlags[flag/16] &= ~(1u << (flag%16));
-    }
+    globalFlags[flag/16] &= ~(1u << (flag%16));
   }
 
   else if(flag <= LAST_LOCAL_FLAG) { // Local flag
@@ -480,17 +428,7 @@ void fnFlipFlag(uint16_t flag) {
   }
 
   else if(flag <= FLAG_K) { // Global flag
-    switch(flag) {
-      case FLAG_A: flipSystemFlag(FLAG_ENGOVR);   break;
-      case FLAG_B: flipSystemFlag(FLAG_OVERFLOW); break;
-      case FLAG_C: flipSystemFlag(FLAG_CARRY);    break;
-      case FLAG_D: flipSystemFlag(FLAG_SPCRES);   break;
-      case FLAG_I: flipSystemFlag(FLAG_CPXRES);   break;
-      case FLAG_L: flipSystemFlag(FLAG_LEAD0);    break;
-      case FLAG_T: flipSystemFlag(FLAG_TRACE);    break;
-      case FLAG_X: flipSystemFlag(FLAG_POLAR);    break;
-      default:     globalFlags[flag/16] ^=  1u << (flag%16);
-    }
+    globalFlags[flag/16] ^=  1u << (flag%16);
   }
 
   else if(flag <= LAST_LOCAL_FLAG) { // Local flag
@@ -532,7 +470,7 @@ void fnFlipFlag(uint16_t flag) {
 
 
 /********************************************//**
- * \brief Clear all global from 00 to 99 and the local flags
+ * \brief Clear all global flags and the local flags
  *
  * \param[in] flags uint16_t
  * \return void
@@ -542,13 +480,10 @@ void fnClFAll(uint16_t confirmation) {
     setConfirmationMode(fnClFAll);
   }
   else {
-    // Leave lettered flags as they are
-    memset(globalFlags, 0, sizeof(globalFlags) - 2*sizeof(uint16_t)); // Clear flags from 00 to 95
-    globalFlags[6] &= 0xfff0; // Clear flags from 96 to 99
-    globalFlags[7] = 0;       // Clear flags from M to W
+    memset(globalFlags, 0, sizeof(globalFlags)); // Clear global flags from 00 to 99 and X to W
 
     if(currentLocalFlags != NULL) {
-      currentLocalFlags->localFlags = 0;
+      currentLocalFlags->localFlags = 0;         // Clear local flags
     }
   }
 }
