5ddd3a92a7adf52ce9838c8f4560914d0abd6ce3
Pauli <ppzgs1@gmail.com>
2024-12-14 21:36:17 +1100

    update parallel to use helpers

--- src/c47/mathematics/parallel.h
+++ src/c47/mathematics/parallel.h
@@ -8,25 +8,4 @@
   #define PARALLEL_H
 
   void fnParallel      (uint16_t unusedButMandatoryParameter);
-
-  #if (EXTRA_INFO_ON_CALC_ERROR == 1)
-    void parallelError   (void);
-  #else // (EXTRA_INFO_ON_CALC_ERROR != 1)
-    #define parallelError typeError
-  #endif // (EXTRA_INFO_ON_CALC_ERROR == 1)
-
-  //      RegYRegX
-  void parallelLonILonI(void);
-  void parallelLonIReal(void);
-  void parallelLonICplx(void);
-
-  //      RegYRegX
-  void parallelRealLonI(void);
-  void parallelRealReal(void);
-  void parallelRealCplx(void);
-
-  //      RegYRegX
-  void parallelCplxLonI(void);
-  void parallelCplxReal(void);
-  void parallelCplxCplx(void);
 #endif // !PARALLEL_H
