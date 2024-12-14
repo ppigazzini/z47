94a0e33a371826526edf026930ed2e83dd1b8ad7
Pauli <ppzgs1@gmail.com>
2024-12-14 21:09:48 +1100

    Update gamma and lngamma to use helper functions

--- src/c47/mathematics/gamma.h
+++ src/c47/mathematics/gamma.h
@@ -10,22 +10,6 @@
   void fnGamma     (uint16_t unusedButMandatoryParameter);
   void fnLnGamma   (uint16_t unusedButMandatoryParameter);
 
-  #if (EXTRA_INFO_ON_CALC_ERROR == 1)
-    void gammaError  (void);
-  #else // (EXTRA_INFO_ON_CALC_ERROR != 1)
-    #define gammaError typeError
-  #endif // (EXTRA_INFO_ON_CALC_ERROR == 1)
-
-  void lnGammaError(void);
-
-  void gammaLonI   (void);
-  void gammaReal   (void);
-  void gammaCplx   (void);
-
-  void lnGammaLonI (void);
-  void lnGammaReal (void);
-  void lnGammaCplx (void);
-
   void complexLnGamma(const real_t *xReal, const real_t *xImag, real_t *rReal, real_t *rImag, realContext_t *realContext);
 
 #endif // !GAMMA_H
