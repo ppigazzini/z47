2adfef443d94142c2dfceb167fa8a546be9aee85
Pauli <ppzgs1@gmail.com>
2024-12-16 11:48:13 +1100

    Convert ln 1+x to use helpers

--- src/c47/mathematics/lnPOne.h
+++ src/c47/mathematics/lnPOne.h
@@ -9,18 +9,5 @@
   // Coded by JM, based on ln.h
 
   void fnLnP1   (uint16_t unusedButMandatoryParameter);
-
-  #if (EXTRA_INFO_ON_CALC_ERROR == 1)
-    void lnP1Error(void);
-  #else // (EXTRA_INFO_ON_CALC_ERROR != 1)
-    #define lnP1Error typeError
-  #endif // (EXTRA_INFO_ON_CALC_ERROR == 1)
-
-  void lnP1LonI (void);
-  void lnP1Rema (void);
-  void lnP1Cxma (void);
-  void lnP1ShoI (void);
-  void lnP1Real (void);
-  void lnP1Cplx (void);
   void lnP1Complex(const real_t *real, const real_t *imag, real_t *lnReal, real_t *lnImag, realContext_t *realContext);
 #endif // !LNPONE_H
