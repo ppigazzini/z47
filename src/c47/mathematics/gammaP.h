d0f0be06cc6187a88d15f1164cf35c3b3492227e
Pauli <ppzgs1@gmail.com>
2024-12-18 20:32:31 +1100

    Update gammaP to use helper functions

--- src/c47/mathematics/gammaP.h
+++ src/c47/mathematics/gammaP.h
@@ -8,15 +8,4 @@
   #define GAMMAP_H
 
   void fnGammaP      (uint16_t unusedButMandatoryParameter);
-
-  #if (EXTRA_INFO_ON_CALC_ERROR == 1)
-    void gammaPError   (void);
-  #else // (EXTRA_INFO_ON_CALC_ERROR != 1)
-    #define gammaPError typeError
-  #endif // (EXTRA_INFO_ON_CALC_ERROR == 1)
-
-  void gammaPLonILonI(void);
-  void gammaPLonIReal(void);
-  void gammaPRealLonI(void);
-  void gammaPRealReal(void);
 #endif // !GAMMAP_H
