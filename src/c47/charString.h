12031cff807497592088d183e00d586da23e48d1
Over_score <martin@lorang.net>
2025-01-24 12:46:20 +0100

    Back to stringCopy()

--- src/c47/charString.h
+++ src/c47/charString.h
@@ -104,8 +104,7 @@ uint32_t utf8ToCodePoint  (const uint8_t *utf8, uint32_t *codePoint);
  * \param[in] source
  * \return a pointer to the end (i.e. terminating null byte) of the resulting string dest
  */
-//char    *stringCopy            (char *dest, const char *source);
-#define stringCopy(dest, source) stpcpy(dest, source)
+char    *stringCopy            (char *dest, const char *source);
 
 void     expandConversionName  (char *msg1);
 void     compressConversionName(char *msg1);
