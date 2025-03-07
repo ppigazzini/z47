cee5cc981aa8801bfdc9093bf4569892e81cc4f4
Jaco <>
2025-03-07 13:39:42 +0200

    Add [V->..yx], optimize stk->V and V->stk; Add V3->zyx and V2->yx to CAT only; menu change;

--- src/c47/softmenus.c
+++ src/c47/softmenus.c
@@ -131,9 +131,9 @@ TO_QSPI const int16_t menu_MATX[]        = {
                                              ITM_CONCAT,                    ITM_M_RR,                   ITM_M_DIM,                ITM_M_DIMQ,            ITM_INDEX,                   ITM_M_EDIN,
                                              ITM_M_PUT,                     ITM_M_GET,                  ITM_STOEL,                ITM_RCLEL,             ITM_STOELPLUS,               ITM_RCLELPLUS,              
 
-                                             ITM_DRG,                       ITM_MULPI2,                 ITM_V3TOSPH,              ITM_V3TOCYL,           ITM_STKTO1x3,                ITM_1x3TOSTK,
+                                             ITM_DRG,                       ITM_MULPI2,                 ITM_V3TOSPH,              ITM_V3TOCYL,           ITM_STKTO1x2,                ITM_STKTO1x3,
                                              ITM_VVDIST,                    ITM_UNITV,                  ITM_ENORM,                ITM_VANGLE,            ITM_DOT_PROD,                ITM_CROSS_PROD,
-                                             ITM_toREC2,                    ITM_toPOL2,                 ITM_NULL,                 ITM_V100,              ITM_V010,                    ITM_V001};
+                                             ITM_toREC2,                    ITM_toPOL2,                 ITM_VECTOSTK,             ITM_V100,              ITM_V010,                    ITM_V001};
 
 //new 3D: ITM_V3TOCYL, ITM_V3TOSPH, ITM_STKTO1x3, ITM_1x3TOSTK
 //new 2D 3D: ITM_VVDIST, ITM_VVANG
