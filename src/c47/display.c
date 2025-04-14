2536f12936d6fcc832d25fe699f6ddeb7e61eb56
Pauli <ppzgs1@gmail.com>
2025-04-14 11:25:15 +1000

    switch from a copy to a pointer in the display IRFRAC code

--- src/c47/display.c
+++ src/c47/display.c
@@ -398,8 +398,8 @@ overRange:
   //printReal34ToConsole(real34, "Irfrac: ","\n");
   if(limitIrfrac != NOIRFRAC) {
     if(getSystemFlag(FLAG_IRFRAC) && IrFractionsCurrentStatus != CF_OFF && !real34CompareAbsLessThan(real34,const34_1e_24) && !real34IsAnInteger(real34)) {      // LIMITIRFRAC [Real Matrixes, Complex Matrixes] & LIGHTIRFRAC [Vectors] for USB/BAT/SIM; FULLIRFRAC [Real, Complex] : pure fractions
-      real_t toleranceIrrational;
-      realCopy(const_1e_24, &toleranceIrrational);
+      const real_t *toleranceIrrational = const_1e_24;
+
       if(checkForAndChange(    displayString, real34, const_1,     &toleranceIrrational, "",                                 frontSpace, complex)) return_fr;
       if((limitIrfrac >= LIMITIRFRAC && runningOnSimOrUSB) || limitIrfrac == FULLIRFRAC) {                                                                       // LIMITIRFRAC [Real Matrixes, Complex Matrixes] & LIGHTIRFRAC [Vectors] for USB/   /SIM; FULLIRFRAC [Real, Complex] : √3, pi, e, √2
         if(checkForAndChange(  displayString, real34, const_rt3,   &toleranceIrrational, STD_SQUARE_ROOT STD_SUB_3,          frontSpace, complex)) return_fr;
@@ -412,10 +412,7 @@ overRange:
           if(checkForAndChange(displayString, real34, const_rt7,   &toleranceIrrational, STD_SQUARE_ROOT STD_SUB_7,          frontSpace, complex)) return_fr;
           if(checkForAndChange(displayString, real34, const_rtpi,  &toleranceIrrational, STD_SQUARE_ROOT STD_pi,             frontSpace, complex)) return_fr;
           if(checkForAndChange(displayString, real34, const_1onpi, &toleranceIrrational, oneOverPi,                          frontSpace, complex)) return_fr;
-          real_t tmp;
-          realCopy(const__1oneE,&tmp);
-          realSetPositiveSign(&tmp);
-          if(checkForAndChange(displayString, real34, &tmp,        &toleranceIrrational, oneOverE,                           frontSpace, complex)) return_fr;
+          if(checkForAndChange(displayString, real34, const_1oneE, &toleranceIrrational, oneOverE,                           frontSpace, complex)) return_fr;
         }
       }
     }
