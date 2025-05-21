1d2415223b99fd5ccb916db7cfb583ddce9dce4f
Pauli <ppzgs1@gmail.com>
2025-05-21 13:14:07 +1000

    Convert integer test verbs to use getRegisterAsLongIntQuiet()

--- src/c47/mathematics/int.c
+++ src/c47/mathematics/int.c
@@ -9,39 +9,11 @@
 
 void fnCheckInteger(uint16_t mode) {
   longInteger_t x;
+  bool_t frac;
 
-  switch(getRegisterDataType(REGISTER_X)) {
-    case dtShortInteger:
-      convertShortIntegerRegisterToLongInteger(REGISTER_X, x);
-      break;
-
-    case dtLongInteger:
-      convertLongIntegerRegisterToLongInteger(REGISTER_X, x);
-      break;
-
-    case dtComplex34:
-      if (!real34IsZero(REGISTER_IMAG34_DATA(REGISTER_X))) {
-        temporaryInformation = TI_FALSE;
-        return;
-      }
-      /* FALL THROUGH */
-    case dtDate:
-    case dtTime:
-    case dtReal34:
-      if (real34IsSpecial(REGISTER_REAL34_DATA(REGISTER_X))) {
-        temporaryInformation = TI_FALSE;
-        return;
-      }
-      if (!real34IsAnInteger(REGISTER_REAL34_DATA(REGISTER_X))) {
-        temporaryInformation = mode == CHECK_INTEGER_FP;
-        return;
-      }
-      convertReal34ToLongInteger(REGISTER_REAL34_DATA(REGISTER_X), x, DEC_ROUND_FLOOR);
-      break;
-
-    default:
-      temporaryInformation = TI_FALSE;
-      return;
+  if (!getRegisterAsLongIntQuiet(REGISTER_X, x, &frac) || frac) {
+    temporaryInformation = TI_FALSE;
+    return;
   }
 
   #if defined(DMCP_BUILD)
@@ -49,6 +21,7 @@ void fnCheckInteger(uint16_t mode) {
   #else // !DMCP_BUILD
     refreshLcd(NULL);
   #endif // DMCP_BUILD
+
   switch(mode) {
     case CHECK_INTEGER: {
       temporaryInformation = TI_TRUE;
