3130a195c7eb06071636769993e5b3850dc6fdf1
Jaco <>
2025-08-30 22:28:42 +0200

    centralize the drawSinglePixelLine() function to easier find the horizontal lines on screen

--- src/c47/browsers/registerBrowser.c
+++ src/c47/browsers/registerBrowser.c
@@ -194,7 +194,7 @@
 
           if(   (regist <  REGISTER_X && regist % 5 == 4)
             || (regist >= REGISTER_X && regist % 4 == 3)) {
-            lcd_fill_rect(0, 218 - 22 * row, SCREEN_WIDTH, 1, LCD_EMPTY_VALUE);
+            drawSinglePixelLine(218 - 22 * row);
           }
 
           _showRegisterInRbr(regist, registerNameWidth);
@@ -215,7 +215,7 @@
             registerNameWidth = showString(tmpString, &standardFont, 1, 219 - 22 * row, vmNormal, true, true);
 
             if(regist % 5 == 1) {
-              lcd_fill_rect(0, 218 - 22 * row, SCREEN_WIDTH, 1, LCD_EMPTY_VALUE);
+              drawSinglePixelLine(218 - 22 * row);
             }
 
             _showRegisterInRbr(regist, registerNameWidth);
@@ -242,7 +242,7 @@
           registerNameWidth = showString(tmpString, &standardFont, 1, 219 - 22 * row, vmNormal, true, true);
 
           if((regist % 5 == 1) || (regist == FIRST_NAMED_VARIABLE + numberOfNamedVariables - 1)) {
-            lcd_fill_rect(0, 218 - 22 * row, SCREEN_WIDTH, 1, LCD_EMPTY_VALUE);
+            drawSinglePixelLine(218 - 22 * row);
           }
 
           _showRegisterInRbr(regist, registerNameWidth);
@@ -262,7 +262,7 @@
             registerNameWidth = showString(tmpString, &standardFont, 1, 219 - 22 * row, vmNormal, true, true);
 
             if(regist % 5 == 1) {
-              lcd_fill_rect(0, 218 - 22 * row, SCREEN_WIDTH, 1, LCD_EMPTY_VALUE);
+              drawSinglePixelLine(218 - 22 * row);
             }
 
             _showRegisterInRbr(regist, registerNameWidth);
