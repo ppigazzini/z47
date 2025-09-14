b647d8a0e79b2d33a088baead4fd64e85c4e2bbb
Didier <didier.lachieze@libertysurf.fr>
2025-09-14 16:00:20 +0200

    Fix conflic with 00.109.02.07a12-internal

--- src/c47/c47Extensions/addons.c
+++ src/c47/c47Extensions/addons.c
@@ -294,8 +294,11 @@ void fnEdit (uint16_t unusedParamButMandatory) {
   //fnEdit: this is simply the stub with the currently working edit routines, linked via ITM_EDIT, which is also located on long press Backspace.
   //All might have to be changed have a propoer generic EDIT function.
   #if !defined(TESTSUITE_BUILD)
-    #if !defined(SAVE_SPACE_DM42_22_EDIT1) || !defined(SAVE_SPACE_DM42_23_EDIT2)
+    #if !defined(SAVE_SPACE_DM42_22_EDIT1)
       int16_t index;
+    #endif //!SAVE_SPACE_DM42_22_EDIT1
+    
+    #if !defined(SAVE_SPACE_DM42_22_EDIT1) || !defined(SAVE_SPACE_DM42_23_EDIT2)
       uint8_t grpGroupingLeftOld;
       uint8_t grpGroupingRightOld;
     #endif
