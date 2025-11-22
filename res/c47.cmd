f2f545dc7f03eb61aa577523bb479cfccbd86bad
Jaco <>
2025-11-22 21:39:59 +0200

    Windows cmd files R47 update

--- res/c47.cmd
+++ res/c47.cmd
@@ -1,11 +1,13 @@
 cd c:\C47
 start "" "c47.exe"
 
+rem to see the options, use c47.exe --help
+
 rem Examples of command line parameters
 rem start "" "c47.exe" "--functionkeys"
-rem start "" "c47.exe" "--c47" "--auto"
-rem start "" "c47.exe" "--c47" "--portrait" "--functionkeys"
-rem start "" "c47.exe" "--c47" "--landscape" "--functionkeys"
-rem start "" "c47.exe" "--r47v0" "--auto" "--functionkeys"
-rem start "" "c47.exe" "--r47v1" "--auto" "--functionkeys"
-rem start "" "c47.exe" "--r47v2" "--auto" "--functionkeys" "--background" "R47custom.png"
+rem start "" "c47.exe" "--auto"
+rem start "" "c47.exe" "--portrait" "--functionkeys"
+rem start "" "c47.exe" "--landscape" "--functionkeys"
+rem start "" "c47.exe" "--auto" "--functionkeys"
+rem start "" "c47.exe" "--auto" "--functionkeys"
+rem start "" "c47.exe" "--auto" "--functionkeys" "--background" "R47custom.png"
