baf8b5fada0d584e6dc3b45b5536d4ff77927921
Jolbas <bjorn@jadelius.se>
2026-01-05 17:12:24 +0100

    20% grey on user keymap

--- src/c47/browsers/asnBrowser.c
+++ src/c47/browsers/asnBrowser.c
@@ -125,7 +125,7 @@
         ) {
         void (*setPixel)(uint32_t,uint32_t) = videoMode ? &setWhitePixel : &setBlackPixel;
         for(int16_t yStroke = YOFF+yy*SOFTMENU_HEIGHT + 3; yStroke < YOFF+(yy+1)*SOFTMENU_HEIGHT - 2; yStroke+=1){
-          for(int16_t xStroke = x1 + 3 + (3*yStroke+x1)%8; xStroke < x2 - 2; xStroke+=8) {
+          for(int16_t xStroke = x1 + 3 + (2*yStroke+x1)%5; xStroke < x2 - 2; xStroke+=5) {
               setPixel(xStroke, yStroke);
           }
         }
