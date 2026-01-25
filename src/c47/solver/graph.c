2a4912ba6006f9c79399684d30cbccda8de4223d
Jolbas <bjorn@jadelius.se>
2026-01-25 15:35:01 +0100

    sat max tolerance to 1e-5

--- src/c47/solver/graph.c
+++ src/c47/solver/graph.c
@@ -1438,9 +1438,8 @@ static inline void copyComplex(const cplx_t *from, cplx_t *to) {
 
     // set tolerance from significantDigits and use higher prcision in execute_rpn_function();
     uint16_t signDig = significantDigits?significantDigits:34;
-    convergenceTolerence(&tol);
-    // realCopy(const_1, &tol);
-    // tol.exponent -= signDig;
+    realCopy(const_1, &tol);
+    tol.exponent -= signDig <= 5 ? 5 : (signDig > 33 ? 33 : signDig);
     fnSetSignificantDigits(34);
 
     convertComplexToResultRegister(CPLX(X0), REGISTER_X); //determined third starting point using the slope or secant
