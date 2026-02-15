b18e40c08547ff171f8396c9b6b758be8929116c
Jaco <>
2026-02-15 22:42:21 +0200

    Version

--- src/c47/defines.h
+++ src/c47/defines.h
@@ -9,7 +9,7 @@
 // VARIOUS OPTIONS
 //*********************************
 
-#define VERSION1 "0.109.03.00a2"       // major release . minor release . tracked build . internal OR un/tracked OR subrelease : Alpha / Beta / RC1
+#define VERSION1 "0.109.03.00b2"       // major release . minor release . tracked build . internal OR un/tracked OR subrelease : Alpha / Beta / RC1
 #define DEVPROFILES
 
 // Version 0.109.02.07b11   Public Release C47 & R47
@@ -19,6 +19,7 @@
 // Version 0.109.03.00a1    Internal C47 & R47
 // Version 0.109.03.00b1    Public C47 & R47, with 2 packages for DM42
 // Version 0.109.03.00a2    Internal C47 & R47
+// Version 0.109.03.00b2    Public C47 & R47
 
 
 #if !defined(CALCMODEL)
