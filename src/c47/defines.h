7ab8f1c0ba0af59d30c7933030ab67a875489a8a
Jaco <>
2026-02-16 01:25:00 +0200

    solve rebase conflict due to changed defines.h in master

--- src/c47/defines.h
+++ src/c47/defines.h
@@ -9,7 +9,7 @@
 // VARIOUS OPTIONS
 //*********************************
 
-#define VERSION1 "0.109.03.00b2"       // major release . minor release . tracked build . internal OR un/tracked OR subrelease : Alpha / Beta / RC1
+#define VERSION1 "0.109.03.00b2.p1"       // major release . minor release . tracked build . internal OR un/tracked OR subrelease : Alpha / Beta / RC1
 #define DEVPROFILES
 
 // Version 0.109.02.07b11   Public Release C47 & R47
