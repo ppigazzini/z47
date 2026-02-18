4c07c046212c608329f6e7948e00b2eeab2b310a
Didier <didier.lachieze@libertysurf.fr>
2026-02-19 00:30:00 +0100

    Rewrite fix for END and remove hourglass for EDIT

--- src/c47/c47Extensions/addons.c
+++ src/c47/c47Extensions/addons.c
@@ -637,7 +637,7 @@ void fnEdit (uint16_t unusedParamButMandatory) {
         break;
 
       case CM_PEM : {
-        if((pemCursorIsZerothStep) || isAtEndOfPrograms(currentStep) || (*currentStep == ((ITM_END >> 8) | 0x80) && *(currentStep + 1) == (ITM_END & 0xff))) return; // Don't try to edit step 000 or END or .END.
+        if((pemCursorIsZerothStep) || isAtEndOfProgram(currentStep) || isAtEndOfPrograms(currentStep)) return; // Don't try to edit step 000 or END or .END.
         //printf("**[DL]** currentLocalStepNumber %d\n",currentLocalStepNumber);fflush(stdout);
         int16_t i = 0;
         int16_t func = currentStep[i++];
