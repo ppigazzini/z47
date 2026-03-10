7eb8afc0ea29cf8501dcbb2692212ee6d5080de3
Jaco <>
2026-03-11 01:57:51 +0200

    fix-TAM-alpha-longpress-ENTER

--- src/c47/c47Extensions/keyboardTweak.c
+++ src/c47/c47Extensions/keyboardTweak.c
@@ -403,7 +403,10 @@ void resetKeytimers(void) {
       #else //TAMALPHA_f
         tmpg_ = getSystemFlag(FLAG_USER) ? kbd_usr[key_no].gShiftedAim : kbd_std[key_no].gShiftedAim;
       #endif //TAMALPHA_f
-      if(   ((key_no != 32 && tmpp_ != ITM_SHIFTf && tmpp_ != ITM_SHIFTg && tmpp_ != KEY_fg && tmpp_ != ITM_BACKSPACE) && (LongPressM == RBX_M1234 || LongPressM == RBX_M124))  //any mathkeys
+      if(   ((key_no != 32 && tmpp_ != ITM_SHIFTf && tmpp_ != ITM_SHIFTg && tmpp_ != KEY_fg && tmpp_ != ITM_BACKSPACE) && 
+            (LongPressM == RBX_M1234 || LongPressM == RBX_M124) &&  //any mathkeys
+            !((key_no == 12 || key_no == 36) && tam.mode == TM_LABEL)
+            )
         ) {
         if(!shiftF && !shiftG) {
           #if defined(TAMALPHA_f)
