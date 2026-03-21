24451e609ce1f074cbebf64c556cac789246065e
Over_score <martin@lorang.net>
2026-03-21 11:42:31 +0100

    A better way to convert real_t to int

--- src/c47/realType.c
+++ src/c47/realType.c
@@ -7,38 +7,35 @@
 
 #include "c47.h"
 
-static void roundToIntAndNormalise(const real_t *r, real_t *i) {
-  realToIntegralValue(r, i, DEC_ROUND_DOWN, &ctxtReal39);
-  /* Adding zero forces the normalisation of the resulting real which is
-   * required for the conversions to integers to work properly
-   */
-  realAdd(const_0, i, i, &ctxtReal39);
+static void roundToIntAndQuantize(const real_t *r, real_t *i) {
+  realToIntegralValue(r, i, DEC_ROUND_DOWN, &ctxtReal39); // After this call, it's guaranteed that i is an integer and i->exponent >= 0
+  decNumberQuantize(i, i, const_0, &ctxtReal39); // After this call, it's guaranteed that the value of i is not changed and i->exponent == 0 (const_0 is used only because it's exponent is 0)
 }
 
 int32_t realToInt32C47(const real_t *r) {
   real_t i;
 
-  roundToIntAndNormalise(r, &i);
+  roundToIntAndQuantize(r, &i);
   return decNumberToInt32(&i, &ctxtReal39);
 }
 
 uint32_t realToUint32C47(const real_t *r) {
   real_t i;
 
-  roundToIntAndNormalise(r, &i);
+  roundToIntAndQuantize(r, &i);
   return decNumberToUInt32(&i, &ctxtReal39);
 }
 
 //int64_t realToInt64C47(const real_t *r) {
 //  real_t i;
 //
-//  roundToIntAndNormalise(r, &i);
+//  roundToIntAndQuantize(r, &i);
 //  return decNumberToInt64(&i, &ctxtReal39);
 //}
 
 uint64_t realToUint64C47(const real_t *r) {
   real_t i;
 
-  roundToIntAndNormalise(r, &i);
+  roundToIntAndQuantize(r, &i);
   return decNumberToUInt64(&i, &ctxtReal39);
 }
