de3d1d357d0d57286e565d052cd3bc186be4d50d
Pasquale Pigazzini <pasquale.pigazzini@gmail.com>
2026-05-23 00:31:46 +0200

    host(gtk): slice B close hal retained include wiring

--- zig_build/host/gtk_gui_rewrites.zig
+++ zig_build/host/gtk_gui_rewrites.zig
@@ -3,21 +3,13 @@ const std = @import("std");
 const retained_gtk_sources = [_][]const u8{
     "c47-gtk.c",
     "gtkGui.c",
-    "hal/audio.c",
-    "hal/gui.c",
     "hal/io.c",
-    "hal/lcd.c",
-    "hal/print_ir.c",
 };
 
 const runtime_helper_sources = [_][]const u8{
     "zig_build/host/gtk_c47_gtk_retained.c",
     "zig_build/host/gtk_gui_retained.c",
-    "zig_build/host/gtk_hal_audio_retained.c",
-    "zig_build/host/gtk_hal_gui_retained.c",
     "zig_build/host/gtk_hal_io_retained.c",
-    "zig_build/host/gtk_hal_lcd_retained.c",
-    "zig_build/host/gtk_hal_print_ir_retained.c",
 };
 
 pub fn filterGtkSources(b: *std.Build, gtk_sources: [][]const u8) ![][]const u8 {
