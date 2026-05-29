b64527ca5ea81c36881f47b5b14d69017aa9e589
Pasquale Pigazzini <pasquale.pigazzini@gmail.com>
2026-05-29 23:42:40 +0200

    refactor(math): extract transform command boundary
    Move the square, cube, square-root, and cube-root command family out of
    math_command_wrappers into a dedicated owned module while keeping the public
    wrapper exports and the retained square-root matrix fallback unchanged. Preserve
    the existing parity behavior for fnSquare, fnCube, fnSquareRoot, and fnCubeRoot
    and validate the extraction with zig build math_command_wrappers_parity.

---
+++ zig_src/mathematics/math_transform_command_owned.zig
@@ -0,0 +1,348 @@
+const runtime = @import("math_command_wrappers_runtime.zig");
+
+const square_root_retained = runtime.retained.z47_math_wrappers_retained_fnSquareRoot;
+
+fn shortIntegerData(reg: runtime.calcRegister_t) *align(1) u64 {
+    return @as(*align(1) u64, @ptrCast(runtime.getRegisterDataPointer(reg).?));
+}
+
+fn squareLonI() callconv(.c) void {
+    var x: runtime.longInteger_t = undefined;
+
+    if (!runtime.getRegisterAsLongInt(runtime.REGISTER_X, &x[0], null)) {
+        return;
+    }
+    defer runtime.__gmpz_clear(&x[0]);
+
+    runtime.__gmpz_mul(&x[0], &x[0], &x[0]);
+    runtime.convertLongIntegerToLongIntegerRegister(&x[0], runtime.REGISTER_X);
+}
+
+fn squareShoI() callconv(.c) void {
+    shortIntegerData(runtime.REGISTER_X).* = runtime.WP34S_intMultiply(
+        shortIntegerData(runtime.REGISTER_X).*,
+        shortIntegerData(runtime.REGISTER_X).*,
+    );
+}
+
+fn squareReal() callconv(.c) void {
+    var x: runtime.real_t = undefined;
+
+    if (!runtime.getRegisterAsReal(runtime.REGISTER_X, &x)) {
+        return;
+    }
+
+    if (runtime.realIsInfinite(&x) and !runtime.getSystemFlag(runtime.FLAG_SPCRES)) {
+        runtime.z47_math_wrappers_report_square_real_domain_error();
+        return;
+    }
+
+    runtime.realMultiply(&x, &x, &x, &runtime.ctxtReal39);
+    runtime.convertRealToResultRegister(&x, runtime.REGISTER_X, runtime.amNone);
+}
+
+fn squareCplx() callconv(.c) void {
+    var a: runtime.real_t = undefined;
+    var b: runtime.real_t = undefined;
+
+    if (!runtime.getRegisterAsComplex(runtime.REGISTER_X, &a, &b)) {
+        return;
+    }
+
+    runtime.mulComplexComplex(&a, &b, &a, &b, &a, &b, &runtime.ctxtReal39);
+    runtime.convertComplexToResultRegister(&a, &b, runtime.REGISTER_X);
+}
+
+fn cubeLonI() callconv(.c) void {
+    var x: runtime.longInteger_t = undefined;
+    var cube_value: runtime.longInteger_t = undefined;
+
+    if (!runtime.getRegisterAsLongInt(runtime.REGISTER_X, &x[0], null)) {
+        return;
+    }
+    defer runtime.__gmpz_clear(&x[0]);
+
+    runtime.__gmpz_init(&cube_value[0]);
+    defer runtime.__gmpz_clear(&cube_value[0]);
+
+    runtime.__gmpz_mul(&cube_value[0], &x[0], &x[0]);
+    runtime.__gmpz_mul(&cube_value[0], &cube_value[0], &x[0]);
+    runtime.convertLongIntegerToLongIntegerRegister(&cube_value[0], runtime.REGISTER_X);
+}
+
+fn cubeShoI() callconv(.c) void {
+    const square_value = runtime.WP34S_intMultiply(
+        shortIntegerData(runtime.REGISTER_X).*,
+        shortIntegerData(runtime.REGISTER_X).*,
+    );
+    shortIntegerData(runtime.REGISTER_X).* = runtime.WP34S_intMultiply(
+        square_value,
+        shortIntegerData(runtime.REGISTER_X).*,
+    );
+}
+
+fn cubeReal() callconv(.c) void {
+    var x: runtime.real_t = undefined;
+    var x_squared: runtime.real_t = undefined;
+
+    if (!runtime.getRegisterAsReal(runtime.REGISTER_X, &x)) {
+        return;
+    }
+
+    if (runtime.realIsInfinite(&x) and !runtime.getSystemFlag(runtime.FLAG_SPCRES)) {
+        runtime.z47_math_wrappers_report_cube_real_domain_error();
+        return;
+    }
+
+    runtime.realMultiply(&x, &x, &x_squared, &runtime.ctxtReal39);
+    runtime.realMultiply(&x_squared, &x, &x, &runtime.ctxtReal39);
+    runtime.convertRealToResultRegister(&x, runtime.REGISTER_X, runtime.amNone);
+}
+
+fn cubeCplx() callconv(.c) void {
+    var a: runtime.real_t = undefined;
+    var b: runtime.real_t = undefined;
+    var real_square: runtime.real_t = undefined;
+    var imag_square: runtime.real_t = undefined;
+
+    if (!runtime.getRegisterAsComplex(runtime.REGISTER_X, &a, &b)) {
+        return;
+    }
+
+    runtime.mulComplexComplex(&a, &b, &a, &b, &real_square, &imag_square, &runtime.ctxtReal39);
+    runtime.mulComplexComplex(&real_square, &imag_square, &a, &b, &a, &b, &runtime.ctxtReal39);
+    runtime.convertComplexToResultRegister(&a, &b, runtime.REGISTER_X);
+}
+
+fn sqrtShoI() callconv(.c) void {
+    var sign_value: i32 = 0;
+
+    _ = runtime.WP34S_extract_value(shortIntegerData(runtime.REGISTER_X).*, &sign_value);
+    if (sign_value != 0 and runtime.getFlag(@intCast(runtime.FLAG_CPXRES))) {
+        var value: runtime.real_t = undefined;
+
+        runtime.convertShortIntegerRegisterToReal(runtime.REGISTER_X, &value, &runtime.ctxtReal39);
+        runtime.reallocateRegister(runtime.REGISTER_X, runtime.dtComplex34, 0, runtime.amNone);
+        runtime.realSetPositiveSign(&value);
+        runtime.realSquareRoot(&value, &value, &runtime.ctxtReal39);
+        runtime.convertComplexToResultRegister(runtime.z47_math_wrappers_const_0(), &value, runtime.REGISTER_X);
+        return;
+    }
+
+    shortIntegerData(runtime.REGISTER_X).* = runtime.WP34S_intSqrt(shortIntegerData(runtime.REGISTER_X).*);
+}
+
+fn sqrtReal() callconv(.c) void {
+    var value: runtime.real_t = undefined;
+
+    if (!runtime.getRegisterAsReal(runtime.REGISTER_X, &value)) {
+        return;
+    }
+
+    if (runtime.realIsInfinite(&value) and !runtime.getSystemFlag(runtime.FLAG_SPCRES)) {
+        runtime.displayCalcErrorMessage(runtime.ERROR_ARG_EXCEEDS_FUNCTION_DOMAIN, runtime.ERR_REGISTER_LINE, runtime.REGISTER_X);
+        runtime.moreInfoOnError("In function sqrtReal:", "cannot use infinity as X input of sqrt when flag D is not set", null, null);
+        return;
+    }
+
+    if (!runtime.realIsNegative(&value)) {
+        runtime.realSquareRoot(&value, &value, &runtime.ctxtReal39);
+        runtime.convertRealToResultRegister(&value, runtime.REGISTER_X, runtime.amNone);
+        return;
+    }
+
+    if (runtime.getFlag(@intCast(runtime.FLAG_CPXRES))) {
+        runtime.realSetPositiveSign(&value);
+        runtime.realSquareRoot(&value, &value, &runtime.ctxtReal39);
+        runtime.convertComplexToResultRegister(runtime.z47_math_wrappers_const_0(), &value, runtime.REGISTER_X);
+        return;
+    }
+
+    runtime.displayCalcErrorMessage(runtime.ERROR_ARG_EXCEEDS_FUNCTION_DOMAIN, runtime.ERR_REGISTER_LINE, runtime.REGISTER_X);
+    runtime.moreInfoOnError("In function sqrtReal:", "sqrt does not accept a negative real when flag I is not set", null, null);
+}
+
+fn sqrtLonI() callconv(.c) void {
+    var value: runtime.longInteger_t = undefined;
+
+    if (!runtime.getRegisterAsLongInt(runtime.REGISTER_X, &value[0], null)) {
+        return;
+    }
+    defer runtime.__gmpz_clear(&value[0]);
+
+    if (value[0]._mp_size >= 0) {
+        var rem: runtime.longInteger_t = undefined;
+        var root: runtime.longInteger_t = undefined;
+
+        runtime.__gmpz_init(&rem[0]);
+        defer runtime.__gmpz_clear(&rem[0]);
+        runtime.__gmpz_init(&root[0]);
+        defer runtime.__gmpz_clear(&root[0]);
+
+        runtime.__gmpz_rootrem(&root[0], &rem[0], &value[0], 2);
+        if (rem[0]._mp_size == 0) {
+            runtime.convertLongIntegerToLongIntegerRegister(&root[0], runtime.REGISTER_X);
+            return;
+        }
+    }
+
+    sqrtReal();
+}
+
+fn sqrtCplx() callconv(.c) void {
+    var real_value: runtime.real_t = undefined;
+    var imag_value: runtime.real_t = undefined;
+
+    if (!runtime.getRegisterAsComplex(runtime.REGISTER_X, &real_value, &imag_value)) {
+        return;
+    }
+
+    if (runtime.realIsZero(&imag_value) and runtime.realIsNegative(&real_value)) {
+        runtime.realChangeSign(&real_value);
+        runtime.realSquareRoot(&real_value, &imag_value, &runtime.ctxtReal39);
+        runtime.realSetZero(&real_value);
+    } else if (runtime.realIsZero(&imag_value)) {
+        runtime.realSquareRoot(&real_value, &real_value, &runtime.ctxtReal39);
+        runtime.realSetZero(&imag_value);
+    } else {
+        runtime.realRectangularToPolar(&real_value, &imag_value, &real_value, &imag_value, &runtime.ctxtReal39);
+        runtime.realSquareRoot(&real_value, &real_value, &runtime.ctxtReal39);
+        runtime.realMultiply(&imag_value, runtime.z47_math_wrappers_const_1on2(), &imag_value, &runtime.ctxtReal39);
+        runtime.realPolarToRectangular(&real_value, &imag_value, &real_value, &imag_value, &runtime.ctxtReal39);
+    }
+
+    runtime.convertComplexToResultRegister(&real_value, &imag_value, runtime.REGISTER_X);
+}
+
+fn curtShoI() callconv(.c) void {
+    var value: runtime.real_t = undefined;
+    var cube_root: i32 = 0;
+
+    runtime.convertShortIntegerRegisterToReal(runtime.REGISTER_X, &value, &runtime.ctxtReal39);
+
+    if (runtime.realIsNegative(&value)) {
+        runtime.realSetPositiveSign(&value);
+        runtime.PowerReal(&value, runtime.z47_math_wrappers_const_1on3(), &value, &runtime.ctxtReal39);
+        runtime.realChangeSign(&value);
+    } else {
+        runtime.PowerReal(&value, runtime.z47_math_wrappers_const_1on3(), &value, &runtime.ctxtReal39);
+    }
+
+    cube_root = runtime.realToInt32C47(&value, null);
+    if (cube_root >= 0) {
+        shortIntegerData(runtime.REGISTER_X).* = runtime.WP34S_build_value(@intCast(cube_root), 0);
+    } else {
+        shortIntegerData(runtime.REGISTER_X).* = runtime.WP34S_build_value(@intCast(-cube_root), 1);
+    }
+}
+
+fn curtReal() callconv(.c) void {
+    var value: runtime.real_t = undefined;
+
+    if (!runtime.getRegisterAsReal(runtime.REGISTER_X, &value)) {
+        return;
+    }
+
+    if (runtime.realIsInfinite(&value) and !runtime.getSystemFlag(runtime.FLAG_SPCRES)) {
+        runtime.displayCalcErrorMessage(runtime.ERROR_ARG_EXCEEDS_FUNCTION_DOMAIN, runtime.ERR_REGISTER_LINE, runtime.REGISTER_X);
+        runtime.moreInfoOnError("In function curtReal:", "cannot use infinity as X input of curt when flag D is not set", null, null);
+        return;
+    }
+
+    if (runtime.realIsNegative(&value)) {
+        runtime.realSetPositiveSign(&value);
+        runtime.PowerReal(&value, runtime.z47_math_wrappers_const_1on3(), &value, &runtime.ctxtReal39);
+        runtime.realChangeSign(&value);
+    } else {
+        runtime.PowerReal(&value, runtime.z47_math_wrappers_const_1on3(), &value, &runtime.ctxtReal39);
+    }
+
+    runtime.convertRealToResultRegister(&value, runtime.REGISTER_X, runtime.amNone);
+}
+
+fn curtLonI() callconv(.c) void {
+    var value: runtime.longInteger_t = undefined;
+    var rem: runtime.longInteger_t = undefined;
+    var root: runtime.longInteger_t = undefined;
+
+    if (!runtime.getRegisterAsLongInt(runtime.REGISTER_X, &value[0], null)) {
+        return;
+    }
+    defer runtime.__gmpz_clear(&value[0]);
+
+    runtime.__gmpz_init(&rem[0]);
+    defer runtime.__gmpz_clear(&rem[0]);
+    runtime.__gmpz_init(&root[0]);
+    defer runtime.__gmpz_clear(&root[0]);
+
+    runtime.__gmpz_rootrem(&root[0], &rem[0], &value[0], 3);
+    if (rem[0]._mp_size == 0) {
+        runtime.convertLongIntegerToLongIntegerRegister(&root[0], runtime.REGISTER_X);
+        return;
+    }
+
+    curtReal();
+}
+
+fn curtCplx() callconv(.c) void {
+    var real_value: runtime.real_t = undefined;
+    var imag_value: runtime.real_t = undefined;
+    var magnitude: runtime.real_t = undefined;
+    var angle: runtime.real_t = undefined;
+
+    if (!runtime.getRegisterAsComplex(runtime.REGISTER_X, &real_value, &imag_value)) {
+        return;
+    }
+
+    magnitude = real_value;
+    angle = imag_value;
+
+    if (runtime.realIsZero(&angle)) {
+        if (runtime.realIsNegative(&magnitude)) {
+            runtime.realSetPositiveSign(&magnitude);
+            runtime.PowerReal(&magnitude, runtime.z47_math_wrappers_const_1on3(), &real_value, &runtime.ctxtReal39);
+            runtime.realChangeSign(&real_value);
+        } else {
+            runtime.PowerReal(&magnitude, runtime.z47_math_wrappers_const_1on3(), &real_value, &runtime.ctxtReal39);
+        }
+        runtime.realSetZero(&imag_value);
+    } else {
+        runtime.realRectangularToPolar(&magnitude, &angle, &magnitude, &angle, &runtime.ctxtReal39);
+        runtime.PowerReal(&magnitude, runtime.z47_math_wrappers_const_1on3(), &magnitude, &runtime.ctxtReal39);
+        runtime.realMultiply(&angle, runtime.z47_math_wrappers_const_1on3(), &angle, &runtime.ctxtReal39);
+        runtime.realPolarToRectangular(&magnitude, &angle, &real_value, &imag_value, &runtime.ctxtReal39);
+    }
+
+    runtime.convertComplexToResultRegister(&real_value, &imag_value, runtime.REGISTER_X);
+}
+
+pub fn square(unused_but_mandatory_parameter: u16) void {
+    _ = unused_but_mandatory_parameter;
+
+    runtime.processIntRealComplexMonadicFunction(&squareReal, &squareCplx, &squareShoI, &squareLonI);
+}
+
+pub fn cube(unused_but_mandatory_parameter: u16) void {
+    _ = unused_but_mandatory_parameter;
+
+    runtime.processIntRealComplexMonadicFunction(&cubeReal, &cubeCplx, &cubeShoI, &cubeLonI);
+}
+
+pub fn squareRoot(unused_but_mandatory_parameter: u16) void {
+    const register_type = runtime.getRegisterDataType(runtime.REGISTER_X);
+
+    _ = unused_but_mandatory_parameter;
+
+    if (register_type == runtime.dtReal34Matrix or register_type == runtime.dtComplex34Matrix) {
+        square_root_retained(0);
+        return;
+    }
+
+    runtime.processIntRealComplexMonadicFunction(&sqrtReal, &sqrtCplx, &sqrtShoI, &sqrtLonI);
+}
+
+pub fn cubeRoot(unused_but_mandatory_parameter: u16) void {
+    _ = unused_but_mandatory_parameter;
+
+    runtime.processIntRealComplexMonadicFunction(&curtReal, &curtCplx, &curtShoI, &curtLonI);
+}
