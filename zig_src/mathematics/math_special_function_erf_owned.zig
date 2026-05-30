89d5720759f22a71fb344588c1dad5d7d1802153
Pasquale Pigazzini <pasquale.pigazzini@gmail.com>
2026-05-30 18:03:56 +0200

    refactor(math): extract special erf owner

---
+++ zig_src/mathematics/math_special_function_erf_owned.zig
@@ -0,0 +1,33 @@
+const runtime = @import("math_command_wrappers_runtime.zig");
+
+fn erfReal() callconv(.c) void {
+    var x: runtime.real_t = undefined;
+
+    if (!runtime.getRegisterAsReal(runtime.REGISTER_X, &x)) {
+        return;
+    }
+
+    runtime.WP34S_Erf(&x, &x, &runtime.ctxtReal39);
+    runtime.convertRealToResultRegister(&x, runtime.REGISTER_X, runtime.amNone);
+}
+
+fn erfcReal() callconv(.c) void {
+    var x: runtime.real_t = undefined;
+
+    if (!runtime.getRegisterAsReal(runtime.REGISTER_X, &x)) {
+        return;
+    }
+
+    runtime.WP34S_Erfc(&x, &x, &runtime.ctxtReal39);
+    runtime.convertRealToResultRegister(&x, runtime.REGISTER_X, runtime.amNone);
+}
+
+pub fn fnErf(unused_but_mandatory_parameter: u16) callconv(.c) void {
+    _ = unused_but_mandatory_parameter;
+    runtime.processRealComplexMonadicFunction(&erfReal, null);
+}
+
+pub fn fnErfc(unused_but_mandatory_parameter: u16) callconv(.c) void {
+    _ = unused_but_mandatory_parameter;
+    runtime.processRealComplexMonadicFunction(&erfcReal, null);
+}
--- zig_src/mathematics/math_special_function_sequence_command_owned.zig
+++ zig_src/mathematics/math_special_function_sequence_command_owned.zig
@@ -1,38 +1,14 @@
+const erf_owned = @import("math_special_function_erf_owned.zig");
 const factorial_owned = @import("math_special_function_factorial_owned.zig");
 const fibonacci_owned = @import("math_special_function_fibonacci_owned.zig");
 const ixyz_owned = @import("math_special_function_ixyz_owned.zig");
-const runtime = @import("math_command_wrappers_runtime.zig");
-
-fn erfReal() callconv(.c) void {
-    var x: runtime.real_t = undefined;
-
-    if (!runtime.getRegisterAsReal(runtime.REGISTER_X, &x)) {
-        return;
-    }
-
-    runtime.WP34S_Erf(&x, &x, &runtime.ctxtReal39);
-    runtime.convertRealToResultRegister(&x, runtime.REGISTER_X, runtime.amNone);
-}
-
-fn erfcReal() callconv(.c) void {
-    var x: runtime.real_t = undefined;
-
-    if (!runtime.getRegisterAsReal(runtime.REGISTER_X, &x)) {
-        return;
-    }
-
-    runtime.WP34S_Erfc(&x, &x, &runtime.ctxtReal39);
-    runtime.convertRealToResultRegister(&x, runtime.REGISTER_X, runtime.amNone);
-}
 
 pub fn fnErf(unused_but_mandatory_parameter: u16) callconv(.c) void {
-    _ = unused_but_mandatory_parameter;
-    runtime.processRealComplexMonadicFunction(&erfReal, null);
+    erf_owned.fnErf(unused_but_mandatory_parameter);
 }
 
 pub fn fnErfc(unused_but_mandatory_parameter: u16) callconv(.c) void {
-    _ = unused_but_mandatory_parameter;
-    runtime.processRealComplexMonadicFunction(&erfcReal, null);
+    erf_owned.fnErfc(unused_but_mandatory_parameter);
 }
 
 pub fn fnFactorial(unused_but_mandatory_parameter: u16) callconv(.c) void {
