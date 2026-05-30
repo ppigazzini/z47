5b85e4e91630da9fe486ca929a608ce1068ae75e
Pasquale Pigazzini <pasquale.pigazzini@gmail.com>
2026-05-30 20:04:08 +0200

    refactor(math): split inverse trig real handlers

--- zig_src/mathematics/math_inverse_trig_command_owned.zig
+++ zig_src/mathematics/math_inverse_trig_command_owned.zig
@@ -1,5 +1,6 @@
 const atan_owned = @import("math_atan_owned.zig");
 const inverse_trig_complex_command_owned = @import("math_inverse_trig_complex_command_owned.zig");
+const inverse_trig_real_command_owned = @import("math_inverse_trig_real_command_owned.zig");
 const ln_complex_owned = @import("math_ln_complex_owned.zig");
 const real_trig_owned = @import("math_real_trig_owned.zig");
 const runtime = @import("math_command_wrappers_runtime.zig");
@@ -134,31 +135,7 @@ fn arcsinCplx() callconv(.c) void {
 }
 
 fn arcsinReal() callconv(.c) void {
-    var x: runtime.real_t = undefined;
-
-    if (!runtime.getRegisterAsReal(runtime.REGISTER_X, &x)) {
-        return;
-    }
-
-    if (runtime.realCompareAbsGreaterThan(&x, runtime.z47_math_wrappers_const_1())) {
-        if (runtime.getFlag(@intCast(runtime.FLAG_CPXRES))) {
-            arcsinCplx();
-            return;
-        }
-
-        if (runtime.getSystemFlag(runtime.FLAG_SPCRES)) {
-            runtime.realSetNaN(&x);
-        } else {
-            runtime.z47_math_wrappers_report_arcsin_real_domain_error();
-            return;
-        }
-    } else {
-        real_trig_owned.arcsinReal(&x, &x, &runtime.ctxtReal39);
-        runtime.convertAngleFromTo(&x, runtime.amRadian, runtime.currentAngularMode, &runtime.ctxtReal39);
-    }
-
-    runtime.reallocateRegister(runtime.REGISTER_X, runtime.dtReal34, 0, @intCast(runtime.currentAngularMode));
-    runtime.convertRealToResultRegister(&x, runtime.REGISTER_X, runtime.currentAngularMode);
+    inverse_trig_real_command_owned.arcsinReal();
 }
 
 fn arccosCplx() callconv(.c) void {
@@ -166,59 +143,11 @@ fn arccosCplx() callconv(.c) void {
 }
 
 fn arccosReal() callconv(.c) void {
-    var x: runtime.real_t = undefined;
-
-    if (!runtime.getRegisterAsReal(runtime.REGISTER_X, &x)) {
-        return;
-    }
-
-    if (runtime.realCompareAbsGreaterThan(&x, runtime.z47_math_wrappers_const_1())) {
-        if (runtime.getFlag(@intCast(runtime.FLAG_CPXRES))) {
-            arccosCplx();
-            return;
-        }
-
-        if (runtime.getSystemFlag(runtime.FLAG_SPCRES)) {
-            runtime.realSetNaN(&x);
-        } else {
-            runtime.z47_math_wrappers_report_arccos_real_domain_error();
-            return;
-        }
-    } else {
-        real_trig_owned.arccosReal(&x, &x, &runtime.ctxtReal39);
-        runtime.convertAngleFromTo(&x, runtime.amRadian, runtime.currentAngularMode, &runtime.ctxtReal39);
-    }
-
-    runtime.convertRealToResultRegister(&x, runtime.REGISTER_X, runtime.currentAngularMode);
+    inverse_trig_real_command_owned.arccosReal();
 }
 
 fn arctanReal() callconv(.c) void {
-    var x: runtime.real_t = undefined;
-    var was_negative = false;
-
-    if (!runtime.getRegisterAsReal(runtime.REGISTER_X, &x)) {
-        return;
-    }
-
-    was_negative = runtime.realIsNegative(&x);
-
-    if (runtime.realIsInfinite(&x)) {
-        if (runtime.getSystemFlag(runtime.FLAG_SPCRES)) {
-            copyReal(&x, runtime.z47_math_wrappers_const_90());
-            if (was_negative) {
-                runtime.realChangeSign(&x);
-            }
-            runtime.convertAngleFromTo(&x, runtime.amDegree, runtime.currentAngularMode, &runtime.ctxtReal39);
-        } else {
-            runtime.z47_math_wrappers_report_arctan_real_domain_error();
-            return;
-        }
-    } else {
-        atan_owned.arctanReal(&x, &x, &runtime.ctxtReal39);
-        runtime.convertAngleFromTo(&x, runtime.amRadian, runtime.currentAngularMode, &runtime.ctxtReal39);
-    }
-
-    runtime.convertRealToResultRegister(&x, runtime.REGISTER_X, runtime.currentAngularMode);
+    inverse_trig_real_command_owned.arctanReal();
 }
 
 fn arctanCplx() callconv(.c) void {
@@ -226,14 +155,7 @@ fn arctanCplx() callconv(.c) void {
 }
 
 fn arcsinhReal() callconv(.c) void {
-    var x: runtime.real_t = undefined;
-
-    if (!runtime.getRegisterAsReal(runtime.REGISTER_X, &x)) {
-        return;
-    }
-
-    _ = ArcsinhReal(&x, &x, &runtime.ctxtReal51);
-    runtime.convertRealToResultRegister(&x, runtime.REGISTER_X, runtime.amNone);
+    inverse_trig_real_command_owned.arcsinhReal();
 }
 
 fn arcsinhCplx() callconv(.c) void {
@@ -245,29 +167,7 @@ fn arccoshCplx() callconv(.c) void {
 }
 
 fn arccoshReal() callconv(.c) void {
-    var x: runtime.real_t = undefined;
-
-    if (!runtime.getRegisterAsReal(runtime.REGISTER_X, &x)) {
-        return;
-    }
-
-    if (runtime.realCompareLessThan(&x, runtime.z47_math_wrappers_const_1())) {
-        if (runtime.getFlag(@intCast(runtime.FLAG_CPXRES))) {
-            arccoshCplx();
-            return;
-        }
-
-        if (runtime.getSystemFlag(runtime.FLAG_SPCRES)) {
-            runtime.realSetNaN(&x);
-        } else {
-            runtime.z47_math_wrappers_report_arccosh_real_domain_error();
-            return;
-        }
-    } else {
-        realArcosh(&x, &x, &runtime.ctxtReal75);
-    }
-
-    runtime.convertRealToResultRegister(&x, runtime.REGISTER_X, runtime.amNone);
+    inverse_trig_real_command_owned.arccoshReal();
 }
 
 fn arctanhCplx() callconv(.c) void {
@@ -275,46 +175,7 @@ fn arctanhCplx() callconv(.c) void {
 }
 
 fn arctanhReal() callconv(.c) void {
-    var x: runtime.real_t = undefined;
-    var result: *const runtime.real_t = &x;
-
-    if (!runtime.getRegisterAsReal(runtime.REGISTER_X, &x)) {
-        return;
-    }
-
-    if (runtime.realIsZero(&x)) {
-        result = runtime.z47_math_wrappers_const_0();
-    } else if (runtime.realCompareEqual(&x, runtime.z47_math_wrappers_const_1())) {
-        if (runtime.getSystemFlag(runtime.FLAG_SPCRES)) {
-            result = runtime.z47_math_wrappers_const_plus_infinity();
-        } else {
-            runtime.z47_math_wrappers_report_arctanh_real_positive_one_domain_error();
-            return;
-        }
-    } else if (runtime.realCompareEqual(&x, runtime.z47_math_wrappers_const_minus_1())) {
-        if (runtime.getSystemFlag(runtime.FLAG_SPCRES)) {
-            result = runtime.z47_math_wrappers_const_minus_infinity();
-        } else {
-            runtime.z47_math_wrappers_report_arctanh_real_negative_one_domain_error();
-            return;
-        }
-    } else if (runtime.realCompareAbsGreaterThan(&x, runtime.z47_math_wrappers_const_1())) {
-        if (runtime.getFlag(@intCast(runtime.FLAG_CPXRES))) {
-            arctanhCplx();
-            return;
-        }
-
-        if (runtime.getSystemFlag(runtime.FLAG_SPCRES)) {
-            runtime.realSetNaN(&x);
-        } else {
-            runtime.z47_math_wrappers_report_arctanh_real_domain_error();
-            return;
-        }
-    } else {
-        real_trig_owned.arctanhReal(&x, &x, &runtime.ctxtReal39);
-    }
-
-    runtime.convertRealToResultRegister(result, runtime.REGISTER_X, runtime.amNone);
+    inverse_trig_real_command_owned.arctanhReal();
 }
 
 pub fn fnArcsin(unused_but_mandatory_parameter: u16) callconv(.c) void {
