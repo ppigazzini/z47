6049d8ba8253d3cca3eace1381bbe1407cc4352a
Pasquale Pigazzini <pasquale.pigazzini@gmail.com>
2026-05-30 19:33:31 +0200

    refactor(math): extract trig complex primitive owner

--- zig_src/mathematics/math_command_wrappers.zig
+++ zig_src/mathematics/math_command_wrappers.zig
@@ -31,6 +31,7 @@ const scalar_integer_inspection_command_owned = @import("math_scalar_integer_ins
 const shift_digits_command_owned = @import("math_shift_digits_command_owned.zig");
 const sign_command_owned = @import("math_sign_command_owned.zig");
 const matrix_vector_command_owned = @import("math_matrix_vector_command_owned.zig");
+const trig_complex_primitives_owned = @import("math_trig_complex_primitives_owned.zig");
 const arithmetic_dispatch_command_owned = @import("math_arithmetic_dispatch_command_owned.zig");
 const special_algebraic_command_owned = @import("math_special_algebraic_command_owned.zig");
 const special_function_sequence_command_owned = @import("math_special_function_sequence_command_owned.zig");
@@ -111,15 +112,7 @@ pub export fn sinComplex(
     res_imag: *runtime.real_t,
     real_context: *runtime.realContext_t,
 ) callconv(.c) void {
-    var sina: runtime.real_t = undefined;
-    var cosa: runtime.real_t = undefined;
-    var sinhb: runtime.real_t = undefined;
-    var coshb: runtime.real_t = undefined;
-
-    circular_trig_owned.convertAngleToSinCosTan(real, runtime.amRadian, &sina, &cosa, null, real_context);
-    real_trig_owned.sinhCoshReal(imag, &sinhb, &coshb, real_context);
-    runtime.realMultiply(&sina, &coshb, res_real, real_context);
-    runtime.realMultiply(&cosa, &sinhb, res_imag, real_context);
+    trig_complex_primitives_owned.sinComplex(real, imag, res_real, res_imag, real_context);
 }
 
 pub export fn cosComplex(
@@ -129,99 +122,23 @@ pub export fn cosComplex(
     res_imag: *runtime.real_t,
     real_context: *runtime.realContext_t,
 ) callconv(.c) void {
-    var sina: runtime.real_t = undefined;
-    var cosa: runtime.real_t = undefined;
-    var sinhb: runtime.real_t = undefined;
-    var coshb: runtime.real_t = undefined;
-
-    circular_trig_owned.convertAngleToSinCosTan(real, runtime.amRadian, &sina, &cosa, null, real_context);
-    real_trig_owned.sinhCoshReal(imag, &sinhb, &coshb, real_context);
-    runtime.realMultiply(&cosa, &coshb, res_real, real_context);
-    runtime.realMultiply(&sina, &sinhb, res_imag, real_context);
-    runtime.realChangeSign(res_imag);
+    trig_complex_primitives_owned.cosComplex(real, imag, res_real, res_imag, real_context);
 }
 
 pub export fn sinCosReal(trig_type: runtime.trigType_t) callconv(.c) void {
-    var x: runtime.real_t = undefined;
-    var x_angular_mode: runtime.angularMode_t = undefined;
-
-    if (!runtime.getRegisterAsRealAngle(runtime.REGISTER_X, &x, &x_angular_mode, runtime.ifLongIntegerDoAngleReduction)) {
-        return;
-    }
-
-    if (runtime.realIsSpecial(&x)) {
-        runtime.realSetNaN(&x);
-    } else if (trig_type == runtime.trigSin) {
-        circular_trig_owned.convertAngleToSinCosTan(&x, x_angular_mode, &x, null, null, &runtime.ctxtReal75);
-    } else {
-        circular_trig_owned.convertAngleToSinCosTan(&x, x_angular_mode, null, &x, null, &runtime.ctxtReal75);
-    }
-
-    runtime.convertRealToResultRegister(&x, runtime.REGISTER_X, runtime.amNone);
+    trig_complex_primitives_owned.sinCosReal(trig_type);
 }
 
 pub export fn sinCosCplx(trig_type: runtime.trigType_t) callconv(.c) void {
-    var z_real: runtime.real_t = undefined;
-    var z_imag: runtime.real_t = undefined;
-
-    if (!runtime.getRegisterAsComplex(runtime.REGISTER_X, &z_real, &z_imag)) {
-        return;
-    }
-
-    if (trig_type == runtime.trigSin) {
-        sinComplex(&z_real, &z_imag, &z_real, &z_imag, &runtime.ctxtReal75);
-    } else {
-        cosComplex(&z_real, &z_imag, &z_real, &z_imag, &runtime.ctxtReal75);
-    }
-
-    runtime.convertComplexToResultRegister(&z_real, &z_imag, runtime.REGISTER_X);
+    trig_complex_primitives_owned.sinCosCplx(trig_type);
 }
 
 pub export fn sinhCoshReal(trig_type: runtime.trigType_t) callconv(.c) void {
-    var x: runtime.real_t = undefined;
-
-    if (!runtime.getRegisterAsReal(runtime.REGISTER_X, &x)) {
-        return;
-    }
-
-    if (runtime.realIsInfinite(&x) and !runtime.getSystemFlag(runtime.FLAG_SPCRES)) {
-        runtime.z47_math_wrappers_report_sinh_cosh_real_domain_error();
-        return;
-    }
-
-    if (trig_type == runtime.trigSin) {
-        real_trig_owned.sinhCoshReal(&x, &x, null, &runtime.ctxtReal39);
-    } else {
-        real_trig_owned.sinhCoshReal(&x, null, &x, &runtime.ctxtReal39);
-    }
-
-    runtime.convertRealToResultRegister(&x, runtime.REGISTER_X, runtime.amNone);
+    trig_complex_primitives_owned.sinhCoshReal(trig_type);
 }
 
 pub export fn sinhCoshCplx(trig_type: runtime.trigType_t) callconv(.c) void {
-    var a: runtime.real_t = undefined;
-    var b: runtime.real_t = undefined;
-    var sinha: runtime.real_t = undefined;
-    var cosha: runtime.real_t = undefined;
-    var sinb: runtime.real_t = undefined;
-    var cosb: runtime.real_t = undefined;
-
-    if (!runtime.getRegisterAsComplex(runtime.REGISTER_X, &a, &b)) {
-        return;
-    }
-
-    real_trig_owned.sinhCoshReal(&a, &sinha, &cosha, &runtime.ctxtReal39);
-    circular_trig_owned.convertAngleToSinCosTan(&b, runtime.amRadian, &sinb, &cosb, null, &runtime.ctxtReal39);
-
-    if (trig_type == runtime.trigSin) {
-        runtime.realMultiply(&sinha, &cosb, &a, &runtime.ctxtReal39);
-        runtime.realMultiply(&cosha, &sinb, &b, &runtime.ctxtReal39);
-    } else {
-        runtime.realMultiply(&cosha, &cosb, &a, &runtime.ctxtReal39);
-        runtime.realMultiply(&sinha, &sinb, &b, &runtime.ctxtReal39);
-    }
-
-    runtime.convertComplexToResultRegister(&a, &b, runtime.REGISTER_X);
+    trig_complex_primitives_owned.sinhCoshCplx(trig_type);
 }
 
 pub export fn TanComplex(
@@ -231,26 +148,7 @@ pub export fn TanComplex(
     r_imag: *runtime.real_t,
     real_context: *runtime.realContext_t,
 ) callconv(.c) u8 {
-    var sina: runtime.real_t = undefined;
-    var cosa: runtime.real_t = undefined;
-    var sinhb: runtime.real_t = undefined;
-    var coshb: runtime.real_t = undefined;
-    var numer_real: runtime.real_t = undefined;
-    var numer_imag: runtime.real_t = undefined;
-    var denom_real: runtime.real_t = undefined;
-    var denom_imag: runtime.real_t = undefined;
-
-    circular_trig_owned.convertAngleToSinCosTan(x_real, runtime.amRadian, &sina, &cosa, null, real_context);
-    real_trig_owned.sinhCoshReal(x_imag, &sinhb, &coshb, real_context);
-
-    runtime.realMultiply(&sina, &coshb, &numer_real, real_context);
-    runtime.realMultiply(&cosa, &sinhb, &numer_imag, real_context);
-    runtime.realMultiply(&cosa, &coshb, &denom_real, real_context);
-    runtime.realMultiply(&sina, &sinhb, &denom_imag, real_context);
-    runtime.realChangeSign(&denom_imag);
-
-    runtime.divComplexComplex(&numer_real, &numer_imag, &denom_real, &denom_imag, r_real, r_imag, real_context);
-    return runtime.ERROR_NONE;
+    return trig_complex_primitives_owned.TanComplex(x_real, x_imag, r_real, r_imag, real_context);
 }
 
 pub export fn TanhComplex(
@@ -260,26 +158,7 @@ pub export fn TanhComplex(
     r_imag: *runtime.real_t,
     real_context: *runtime.realContext_t,
 ) callconv(.c) u8 {
-    var sin_value: runtime.real_t = undefined;
-    var cos_value: runtime.real_t = undefined;
-    var denom_real: runtime.real_t = undefined;
-    var denom_imag: runtime.real_t = undefined;
-
-    _ = real_context;
-
-    if (runtime.realIsZero(x_imag)) {
-        real_trig_owned.tanhReal(x_real, r_real, &runtime.ctxtReal39);
-        runtime.realSetZero(r_imag);
-    } else {
-        real_trig_owned.tanhReal(x_real, r_real, &runtime.ctxtReal39);
-        circular_trig_owned.convertAngleToSinCosTan(x_imag, runtime.amRadian, &sin_value, &cos_value, r_imag, &runtime.ctxtReal39);
-
-        runtime.realSetOne(&denom_real);
-        runtime.realMultiply(r_real, r_imag, &denom_imag, &runtime.ctxtReal39);
-        runtime.divComplexComplex(r_real, r_imag, &denom_real, &denom_imag, r_real, r_imag, &runtime.ctxtReal39);
-    }
-
-    return runtime.ERROR_NONE;
+    return trig_complex_primitives_owned.TanhComplex(x_real, x_imag, r_real, r_imag, real_context);
 }
 
 pub export fn realExpLimitCheck(
