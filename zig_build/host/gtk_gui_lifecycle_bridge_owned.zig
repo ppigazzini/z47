91fd9cd1f3cac6deff2a1cadf880c7a1224941ae
Pasquale Pigazzini <pasquale.pigazzini@gmail.com>
2026-05-31 09:26:09 +0200

    refactor(host): split GTK lifecycle configure domain
    Extract configure-event lifecycle routing into gtk_gui_lifecycle_configure_owned
    and narrow bridge scope.

--- zig_build/host/gtk_gui_lifecycle_bridge_owned.zig
+++ zig_build/host/gtk_gui_lifecycle_bridge_owned.zig
@@ -1,11 +1,12 @@
 const lifecycle_owned = @import("gtk_gui_lifecycle_owned.zig");
+const lifecycle_configure_owned = @import("gtk_gui_lifecycle_configure_owned.zig");
 
 pub fn destroyCalc(widget: ?*anyopaque, event: ?*anyopaque, data: ?*anyopaque) c_int {
     return lifecycle_owned.destroyCalc(widget, event, data);
 }
 
 pub fn onConfigureEvent(widget: ?*anyopaque, event: ?*anyopaque, data: ?*anyopaque) c_int {
-    return lifecycle_owned.onConfigureEvent(widget, event, data);
+    return lifecycle_configure_owned.onConfigureEvent(widget, event, data);
 }
 
 pub fn onUiActivity(widget: ?*anyopaque, event: ?*anyopaque, data: ?*anyopaque) c_int {
